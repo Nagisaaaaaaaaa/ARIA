917588bc0352323991ac2a197113e4b3b5317672
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-15 22:01:36 +0800

    add geometry module

---
+++ ARIA/Core/Geometry/include/ARIA/AABB.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "ARIA/Vec.h"
+
+namespace ARIA {}
---
+++ ARIA/Core/Geometry/tests/AABB.cpp
@@ -0,0 +1,9 @@
+#include "ARIA/AABB.h"
+
+#include <gtest/gtest.h>
+
+namespace ARIA {
+
+TEST(AABB, Base) {}
+
+} // namespace ARIA
