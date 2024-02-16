635636d440023b8c260e37b0d9087014d97a71cc
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-16 17:30:58 +0800

    adding morton code

--- ARIA/Core/Math/include/ARIA/MortonCode.h
+++ ARIA/Core/Math/include/ARIA/MortonCode.h
@@ -8,51 +8,6 @@
 /// 3. https://stackoverflow.com/questions/1024754/how-to-compute-a-3d-morton-number-interleave-the-bits-of-3-ints.
 
 #if 0
-// 2D 32
-
-static const unsigned int B[] = {0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF};
-static const unsigned int S[] = {1, 2, 4, 8};
-
-unsigned int x; // Interleave lower 16 bits of x and y, so the bits of x
-unsigned int y; // are in the even positions and bits from y in the odd;
-unsigned int z; // z gets the resulting 32-bit Morton Number.
-                // x and y must initially be less than 65536.
-
-x = (x | (x << S[3])) & B[3];
-x = (x | (x << S[2])) & B[2];
-x = (x | (x << S[1])) & B[1];
-x = (x | (x << S[0])) & B[0];
-
-y = (y | (y << S[3])) & B[3];
-y = (y | (y << S[2])) & B[2];
-y = (y | (y << S[1])) & B[1];
-y = (y | (y << S[0])) & B[0];
-
-z = x | (y << 1);
-
-
-
-// 2D 64
-
-void xy2d_morton(uint64_t x, uint64_t y, uint64_t *d)
-{
-    x = (x | (x << 16)) & 0x0000FFFF0000FFFF;
-    x = (x | (x << 8)) & 0x00FF00FF00FF00FF;
-    x = (x | (x << 4)) & 0x0F0F0F0F0F0F0F0F;
-    x = (x | (x << 2)) & 0x3333333333333333;
-    x = (x | (x << 1)) & 0x5555555555555555;
-
-    y = (y | (y << 16)) & 0x0000FFFF0000FFFF;
-    y = (y | (y << 8)) & 0x00FF00FF00FF00FF;
-    y = (y | (y << 4)) & 0x0F0F0F0F0F0F0F0F;
-    y = (y | (y << 2)) & 0x3333333333333333;
-    y = (y | (y << 1)) & 0x5555555555555555;
-
-    *d = x | (y << 1);
-}
-
-
-
 // 3D 32
 x &= 0x3ff
 x = (x | x << 16) & 0x30000ff   #<<< THIS IS THE MASK for shifting 16 (for bit 8 and 9)
@@ -87,6 +42,29 @@ x = (x | x << 2) & 0x9249249249249249249249249249249L
 namespace ARIA {
 
 template <std::integral I>
-[[nodiscard]] ARIA_HOST_DEVICE I MortonCode(const Vec2<I> &c) {}
+[[nodiscard]] ARIA_HOST_DEVICE I MortonCode(const Vec2<I> &coord) {
+  if constexpr (sizeof(I) == 4) {
+    auto shift = [](I x) {
+      x &= I{0xFFFF};
+      x = (x | (x << 8)) & I{0x00FF00FF};
+      x = (x | (x << 4)) & I{0x0F0F0F0F};
+      x = (x | (x << 2)) & I{0x33333333};
+      x = (x | (x << 1)) & I{0x55555555};
+      return x;
+    };
+    return shift(coord.x()) | (shift(coord.y()) << 1);
+  } else if constexpr (sizeof(I) == 8) {
+    auto shift = [](I x) {
+      x &= I{0xFFFFFFFFLLU};
+      x = (x | (x << 16)) & I{0x0000FFFF0000FFFFLLU};
+      x = (x | (x << 8)) & I{0x00FF00FF00FF00FFLLU};
+      x = (x | (x << 4)) & I{0x0F0F0F0F0F0F0F0FLLU};
+      x = (x | (x << 2)) & I{0x3333333333333333LLU};
+      x = (x | (x << 1)) & I{0x5555555555555555LLU};
+      return x;
+    };
+    return shift(coord.x()) | (shift(coord.y()) << 1);
+  }
+}
 
 } // namespace ARIA
