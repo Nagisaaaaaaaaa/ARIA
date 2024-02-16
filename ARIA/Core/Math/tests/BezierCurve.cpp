bb143c15f09d926e0a92ca2bdabc200390b170dc
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-17 00:30:22 +0800

    .

--- ARIA/Core/Math/tests/BezierCurve.cpp
+++ ARIA/Core/Math/tests/BezierCurve.cpp
@@ -12,6 +12,9 @@ TEST(BezierCurve, Base) {
   static_assert(MovingPoint<BezierCurve, float, 1, Degree<2>, NonRational>);
   static_assert(MovingPoint<BezierCurve, double, 1, Degree<2>, NonRational>);
   static_assert(MovingPoint<BezierCurve, Real, 1, Degree<2>, NonRational>);
+
+  BezierCurve<float, 2, Degree<2>, NonRational> b0;
+  BezierCurve<float, 2, Degree<2>, Rational> b1;
 }
 
 } // namespace ARIA
