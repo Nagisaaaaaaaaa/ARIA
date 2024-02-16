1df941fad0d60fd6335b000de47fefc4640730f1
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-17 00:26:18 +0800

    .

--- ARIA/Core/Math/include/ARIA/BezierCurve.h
+++ ARIA/Core/Math/include/ARIA/BezierCurve.h
@@ -27,10 +27,11 @@ class BezierCurve;
 template <typename T, auto dim, uint degree, typename RationalOrNot>
 class BezierCurve<T, dim, Degree<degree>, RationalOrNot> {
 private:
+  // `CP` is an abbreviation of "control point".
   static constexpr uint nCPs = degree + 1;
   static constexpr bool rational = std::is_same_v<RationalOrNot, Rational>;
   using VecDim = Vec<T, dim>;
-  using VecCP = std::conditional_t<rational, Vec<T, dim + 1>, Vec<T, dim>>;
+  using VecCP = std::conditional_t<rational, Vec<T, dim + 1>, VecDim>;
 
 public:
   BezierCurve() {
@@ -41,27 +42,26 @@ public:
   ARIA_REF_PROP(public, , controlPoints, controlPoints_);
 
 public:
-  constexpr bool IsInDomain(const T &t) const { return T{0} <= t && t <= T{1}; }
+  [[nodiscard]] ARIA_HOST_DEVICE constexpr bool IsInDomain(const T &t) const { return T{0} <= t && t <= T{1}; }
 
-  VecDim operator()(const T &t) const {
+  [[nodiscard]] ARIA_HOST_DEVICE VecDim operator()(const T &t) const {
     ARIA_ASSERT(IsInDomain(t));
 
     // Apply the deCasteljau algorithm, 1997, The NURBS Book, 24.
-
     std::array<VecCP, nCPs> temp;
 
-    for (uint i = 0; i < nCPs; ++i)
-      temp[i] = controlPoints()[i];
-    for (uint round = 1; round <= degree; ++round)
-      for (uint i = 0; i <= degree - round; ++i)
-        temp[i] = Lerp(temp[i], temp[i + 1], t);
+    ForEach<nCPs>([&]<auto i>() { temp[i] = controlPoints()[i]; });
+    ForEach<degree>([&]<auto _r>() {
+      auto round = _r + 1;
+      ForEach<degree - round + 1>([&]<auto i>() { temp[i] = Lerp(temp[i], temp[i + 1], t); });
+    });
 
-    if constexpr (rational) {
-      VecDim position = temp[0].block<dim, 1>(0, 0) / temp[0][dim];
-      return position;
-    } else {
-      return temp[0];
-    }
+    VecDim position;
+    if constexpr (rational)
+      position = temp[0].block<dim, 1>(0, 0) / temp[0][dim];
+    else
+      position = temp[0];
+    return position;
   }
 
 private:
@@ -74,9 +74,9 @@ private:
 template <typename T, auto dim, typename RationalOrNot>
 class BezierCurve<T, dim, DegreeDynamic, RationalOrNot> {
 public:
-  constexpr bool IsInDomain(const T &t) const { return T{0} <= t && t <= T{1}; }
+  [[nodiscard]] ARIA_HOST_DEVICE constexpr bool IsInDomain(const T &t) const { return T{0} <= t && t <= T{1}; }
 
-  Vec<T, dim> operator()(const T &t) const {
+  [[nodiscard]] ARIA_HOST_DEVICE Vec<T, dim> operator()(const T &t) const {
     ARIA_ASSERT(IsInDomain(t));
     // TODO: Implement this.
   }
