8e045edf7cd9ce6195d289b5b0d703f05b39b17f
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-16 19:54:51 +0800

    .

--- ARIA/Core/Math/include/ARIA/MortonCode.h
+++ ARIA/Core/Math/include/ARIA/MortonCode.h
@@ -1,45 +1,94 @@
 #pragma once
 
 /// \file
-/// \brief
+/// \brief Morton curve, or the so-called z-order curve, is a space filling curve.
+/// A space filling curve maps multidimensional data to one dimension while
+/// preserving locality of the data points.
+/// See https://en.wikipedia.org/wiki/Z-order_curve.
+///
+/// This file introduces a Morton code encoder, which can be used to
+/// encode a N-dimensional integral coordinate to an integer.
 
+//
+//
+//
+//
+//
 #include "ARIA/Vec.h"
 
 namespace ARIA {
 
-/// \example ```cpp
-/// using Code2D = MortonCode<2>;
-/// using Code3D = MortonCode<3>;
+/// \brief A Morton code encoder, which can be used to
+/// encode a N-dimensional integral coordinate to an integer.
+/// See https://en.wikipedia.org/wiki/Z-order_curve.
 ///
-/// uint code = Code2D::Encode(Vec2u{5, 6});
-/// uint code = Code3D::Encode(Vec3u{5, 6, 7});
+/// \tparam d Dimension.
+///
+/// \example ```cpp
+/// using Code = MortonCode<2>; // 2D.
+///
+/// uint code0 = Code::Encode(Vec2u{0, 0}); // Will be 0.
+/// uint code1 = Code::Encode(Vec2u{1, 0}); //         1.
+/// uint code2 = Code::Encode(Vec2u{0, 1}); //         2.
+/// uint code3 = Code::Encode(Vec2u{1, 1}); //         3.
+///
+/// using Code = MortonCode<3>; // 3D.
+///
+/// uint code0 = Code::Encode(Vec3u{0, 0, 0}); // Will be 0.
+/// uint code1 = Code::Encode(Vec3u{1, 0, 0}); //         1.
+/// uint code2 = Code::Encode(Vec3u{0, 1, 0}); //         2.
+/// uint code3 = Code::Encode(Vec3u{1, 1, 0}); //         3.
+/// uint code4 = Code::Encode(Vec3u{0, 0, 1}); //         4.
+/// uint code5 = Code::Encode(Vec3u{1, 0, 1}); //         5.
+/// uint code6 = Code::Encode(Vec3u{0, 1, 1}); //         6.
+/// uint code7 = Code::Encode(Vec3u{1, 1, 1}); //         7.
 /// ```
 template <uint d>
 class MortonCode;
 
+//
+//
+//
+//
+//
+// 2D specialization.
 template <>
 class MortonCode<2> {
 public:
-  /// \brief 2D Morton code encoding function.
+  /// \brief Encode a 2D coordinate to Morton code.
   ///
   /// \example ```cpp
-  /// using Code = MortonCode<2>;
+  /// using Code = MortonCode<2>; // 2D.
   ///
-  /// uint code = Code::Encode(Vec2u{5, 6});
+  /// uint code0 = Code::Encode(Vec2u{0, 0}); // Will be 0.
+  /// uint code1 = Code::Encode(Vec2u{1, 0}); //         1.
+  /// uint code2 = Code::Encode(Vec2u{0, 1}); //         2.
+  /// uint code3 = Code::Encode(Vec2u{1, 1}); //         3.
   /// ```
   template <std::integral I>
   [[nodiscard]] static ARIA_HOST_DEVICE /*constexpr*/ I Encode(const Vec2<I> &coord);
 };
 
+//
+//
+//
+// 3D specialization.
 template <>
 class MortonCode<3> {
 public:
-  /// \brief 3D Morton code encoding function.
+  /// \brief Encode a 3D coordinate to Morton code.
   ///
   /// \example ```cpp
-  /// using Code = MortonCode<3>;
+  /// using Code = MortonCode<3>; // 3D.
   ///
-  /// uint code = Code::Encode(Vec3u{5, 6, 7});
+  /// uint code0 = Code::Encode(Vec3u{0, 0, 0}); // Will be 0.
+  /// uint code1 = Code::Encode(Vec3u{1, 0, 0}); //         1.
+  /// uint code2 = Code::Encode(Vec3u{0, 1, 0}); //         2.
+  /// uint code3 = Code::Encode(Vec3u{1, 1, 0}); //         3.
+  /// uint code4 = Code::Encode(Vec3u{0, 0, 1}); //         4.
+  /// uint code5 = Code::Encode(Vec3u{1, 0, 1}); //         5.
+  /// uint code6 = Code::Encode(Vec3u{0, 1, 1}); //         6.
+  /// uint code7 = Code::Encode(Vec3u{1, 1, 1}); //         7.
   /// ```
   template <std::integral I>
   [[nodiscard]] static ARIA_HOST_DEVICE /*constexpr*/ I Encode(const Vec3<I> &coord);
