736fb85ecf4a63f4a991f2a39e856c8c5b1d7473
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-16 14:25:53 +0800

    .

--- ARIA/Core/Geometry/tests/AABB.cpp
+++ ARIA/Core/Geometry/tests/AABB.cpp
@@ -21,28 +21,33 @@ TEST(AABB, Base) {
     EXPECT_FLOAT_EQ(aabb.sup().z(), sup.z());
   };
 
-  // 2D constructors, unionized, and unionize.
+  // 2D constructors, unionized, unionize, and empty.
   {
     // Default constructor.
     AABB2f aabb0;
     expectAABB2(aabb0, {infinity<float>, infinity<float>}, {-infinity<float>, -infinity<float>});
+    EXPECT_TRUE(aabb0.empty());
 
     // Copy constructor.
     AABB2f aabb1 = aabb0;
     expectAABB2(aabb1, {infinity<float>, infinity<float>}, {-infinity<float>, -infinity<float>});
+    EXPECT_TRUE(aabb1.empty());
 
     // Construct from 1 point.
     AABB2f aabb2{Vec2f{0.1F, 0.1F}};
     expectAABB2(aabb2, {0.1F, 0.1F}, {0.1F, 0.1F});
+    EXPECT_FALSE(aabb2.empty());
 
     // Construct from 2 AABBs.
     AABB2f aabb3{Vec2f{0.01F, 0.01F}};
     AABB2f aabb4{aabb2, aabb3};
     expectAABB2(aabb4, {0.01F, 0.01F}, {0.1F, 0.1F});
+    EXPECT_FALSE(aabb4.empty());
 
     // Construct from 1 AABB + 1 point.
     AABB2f aabb5{aabb4, Vec2f{0.001F, 0.001F}};
     expectAABB2(aabb5, {0.001F, 0.001F}, {0.1F, 0.1F});
+    EXPECT_FALSE(aabb5.empty());
 
     // Construct from 3 items.
     Vec2f point{0.001F, 0.001F};
@@ -85,32 +90,66 @@ TEST(AABB, Base) {
 
     AABB2f aabb7 = AABB2f::unionized(Vec2f{0.05F, 0.05F}, Vec2f{0.01F, 0.01F}, aabb2);
     expectAABB2(aabb7, {0.01F, 0.01F}, {0.1F, 0.1F});
+
+    // Complex Empty.
+    {
+      AABB2f aabb;
+      aabb.inf() = {0.1F, 0.2F};
+      aabb.sup() = {0.3F, 0.4F};
+      EXPECT_FALSE(aabb.empty());
+    }
+
+    {
+      AABB2f aabb;
+      aabb.inf() = {0.1F, 0.4F};
+      aabb.sup() = {0.3F, 0.2F};
+      EXPECT_TRUE(aabb.empty());
+    }
+
+    {
+      AABB2f aabb;
+      aabb.inf() = {0.3F, 0.2F};
+      aabb.sup() = {0.1F, 0.4F};
+      EXPECT_TRUE(aabb.empty());
+    }
+
+    {
+      AABB2f aabb;
+      aabb.inf() = {0.3F, 0.4F};
+      aabb.sup() = {0.1F, 0.2F};
+      EXPECT_TRUE(aabb.empty());
+    }
   }
 
-  // 3D constructors, unionized, and unionize.
+  // 3D constructors, unionized, unionize, and empty.
   {
     // Default constructor.
     AABB3f aabb0;
     expectAABB3(aabb0, {infinity<float>, infinity<float>, infinity<float>},
                 {-infinity<float>, -infinity<float>, -infinity<float>});
+    EXPECT_TRUE(aabb0.empty());
 
     // Copy constructor.
     AABB3f aabb1 = aabb0;
     expectAABB3(aabb1, {infinity<float>, infinity<float>, infinity<float>},
                 {-infinity<float>, -infinity<float>, -infinity<float>});
+    EXPECT_TRUE(aabb1.empty());
 
     // Construct from 1 point.
     AABB3f aabb2{Vec3f{0.1F, 0.1F, 0.1F}};
     expectAABB3(aabb2, {0.1F, 0.1F, 0.1F}, {0.1F, 0.1F, 0.1F});
+    EXPECT_FALSE(aabb2.empty());
 
     // Construct from 2 AABBs.
     AABB3f aabb3{Vec3f{0.01F, 0.01F, 0.01F}};
     AABB3f aabb4{aabb2, aabb3};
     expectAABB3(aabb4, {0.01F, 0.01F, 0.01F}, {0.1F, 0.1F, 0.1F});
+    EXPECT_FALSE(aabb4.empty());
 
     // Construct from 1 AABB + 1 point.
     AABB3f aabb5{aabb4, Vec3f{0.001F, 0.001F, 0.001F}};
     expectAABB3(aabb5, {0.001F, 0.001F, 0.001F}, {0.1F, 0.1F, 0.1F});
+    EXPECT_FALSE(aabb5.empty());
 
     // Construct from 3 items.
     Vec3f point{0.001F, 0.001F, 0.001F};
