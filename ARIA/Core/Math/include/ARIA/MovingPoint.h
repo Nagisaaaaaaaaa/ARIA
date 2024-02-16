05d1073340d7b3dba4ac87cbed7bf2d435b010f1
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-16 22:02:40 +0800

    .

--- ARIA/Core/Math/include/ARIA/MovingPoint.h
+++ ARIA/Core/Math/include/ARIA/MovingPoint.h
@@ -20,6 +20,13 @@ namespace ARIA {
 /// Given any `t` satisfying `IsInDomain(t)`,
 /// its `operator()(t)` returns the position at `t`.
 ///
+/// \tparam TMovingPoint A template, whose template parameters will
+/// be substituted with `TValue`, `dim`, and `TOthers...`.
+/// \tparam dim Dimension of the moving point.
+/// Note that if homogeneous coordinate is used, for example, (x, y, z, w).
+/// `dim` should still be `3` at this time.
+/// \tparam TOthers Other template parameters which have no relation with this concept.
+///
 /// \example ```cpp
 /// template <typename T, auto dim, typename degree>
 /// class BezierCurve {
