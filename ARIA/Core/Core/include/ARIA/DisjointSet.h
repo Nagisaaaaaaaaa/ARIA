9fc8082062c81e06a5aa2f28c84892a6db8e39b7
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-19 15:30:51 +0800

    .

--- ARIA/Core/Core/include/ARIA/DisjointSet.h
+++ ARIA/Core/Core/include/ARIA/DisjointSet.h
@@ -2,14 +2,19 @@
 
 #include "ARIA/ARIA.h"
 
+#include <cuda/std/atomic>
+
 namespace ARIA {
 
-template <typename TThreadSafeOrNot, typename TLabels>
-class DisjointSet {
+template <typename TThreadUnsafeOrSafe, typename TLabels>
+class DisjointSet;
+
+template <typename TLabels>
+class DisjointSet<ThreadSafe, TLabels> {
 public:
   DisjointSet() = default;
 
-  DisjointSet(const TLabels &labels) : labels_(labels) {}
+  explicit DisjointSet(const TLabels &labels) : labels_(labels) {}
 
 public:
   template <typename Coord>
@@ -26,3 +31,57 @@ private:
 };
 
 } // namespace ARIA
+
+#if 0
+DEV static ALWAYS_INLINE size_t CCLFind(
+    const std::span<size_t>& ccl,
+    size_t i) {
+  size_t new_i;
+
+  while ((new_i = ccl[i]) != i) {
+    i = new_i;
+  }
+
+  return i;
+}
+
+DEV static ALWAYS_INLINE size_t CCLFindAndCompress(
+    std::span<size_t>& ccl,
+    size_t i) {
+  auto i_c = i;
+
+  size_t new_i;
+
+  while ((new_i = ccl[i]) != i) {
+    i = new_i;
+    ccl[i_c] = i;
+  }
+
+  return i;
+}
+
+DEV static ALWAYS_INLINE void CCLUnion(
+    std::span<size_t>& ccl,
+    size_t i0, size_t i1) {
+  bool done;
+
+  do {
+    i0 = CCLFind(ccl, i0);
+    i1 = CCLFind(ccl, i1);
+
+    if (i0 < i1) {
+      auto old = atomicMin(&ccl[i1], i0);
+      done = (old == i1);
+      i1 = old;
+    }
+    else if (i1 < i0) {
+      auto old = atomicMin(&ccl[i0], i1);
+      done = (old == i0);
+      i0 = old;
+    }
+    else {
+      done = true;
+    }
+  } while (!done);
+}
+#endif
