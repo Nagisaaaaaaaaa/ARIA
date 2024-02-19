ff973680565957d07e1c77e05c7aaf53689278f4
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-19 17:48:54 +0800

    .

--- ARIA/Core/Core/tests/DisjointSet.cu
+++ ARIA/Core/Core/tests/DisjointSet.cu
@@ -66,7 +66,7 @@ TEST(DisjointSet, Base) {
                5, 6, 7, 8, 9,      //
                10, 11, 12, 13, 14, //
                15, 16, 17, 18, 19, //
-               20, 20, 22, 23, 24);
+               20, 21, 22, 23, 24);
 
   disjointSet.Union(crd2Idx(0, 4), crd2Idx(0, 4));
   expectLabels(0, 1, 2, 3, 4,      //
