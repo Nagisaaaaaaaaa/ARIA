9932f92292ab4f503112207e46149f8992218b6a
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-22 02:02:22 +0800

    .

--- ARIA/Core/Core/include/ARIA/Concepts.h
+++ ARIA/Core/Core/include/ARIA/Concepts.h
@@ -27,4 +27,26 @@ static constexpr bool is_invocable_with_brackets_v = is_invocable_with_brackets<
 template <typename T, typename... Ts>
 concept invocable_with_brackets = is_invocable_with_brackets_v<T, Ts...>;
 
+//
+//
+//
+//
+//
+template <typename TRes, typename T, typename... Ts>
+struct is_invocable_with_brackets_r {
+  static constexpr bool value = false;
+};
+
+template <typename TRes, typename T, typename... Ts>
+  requires(sizeof...(Ts) == 1 &&
+           requires(T t, Ts &&...ts) {
+             { t[std::get<0>(std::forward_as_tuple(std::forward<Ts>(ts)...))] } -> std::convertible_to<TRes>;
+           })
+struct is_invocable_with_brackets_r<TRes, T, Ts...> {
+  static constexpr bool value = true;
+};
+
+template <typename TRes, typename T, typename... Ts>
+static constexpr bool is_invocable_with_brackets_r_v = is_invocable_with_brackets_r<TRes, T, Ts...>::value;
+
 } // namespace ARIA
--- ARIA/Core/Core/tests/Concepts.cpp
+++ ARIA/Core/Core/tests/Concepts.cpp
@@ -26,6 +26,7 @@ public:
 } // namespace
 
 TEST(Concepts, Base) {
+  // Is invocable with brackets.
   static_assert(is_invocable_with_brackets_v<std::vector<int>, int>);
   static_assert(is_invocable_with_brackets_v<std::vector<int>, uint64>);
   static_assert(is_invocable_with_brackets_v<std::vector<int>, double>);
@@ -40,6 +41,27 @@ TEST(Concepts, Base) {
 
   static_assert(!is_invocable_with_brackets_v<CallByBrackets, int, int>);
   static_assert(!is_invocable_with_brackets_v<CallByParentheses, int>);
+
+  // Is invocable with brackets (r).
+  static_assert(std::is_invocable_r_v<int, CallByParentheses, int>);
+  static_assert(std::is_invocable_r_v<float, CallByParentheses, int>);
+  static_assert(std::is_invocable_r_v<uint64, CallByParentheses, int>);
+
+  static_assert(is_invocable_with_brackets_r_v<int, std::vector<int>, int>);
+  static_assert(is_invocable_with_brackets_r_v<double, std::vector<int>, uint64>);
+  static_assert(is_invocable_with_brackets_r_v<uint64, std::vector<int>, double>);
+
+  static_assert(is_invocable_with_brackets_r_v<int, CallByBrackets, int>);
+  static_assert(is_invocable_with_brackets_r_v<double, CallByBrackets, uint64>);
+  static_assert(is_invocable_with_brackets_r_v<uint64, CallByBrackets, double>);
+
+  static_assert(is_invocable_with_brackets_r_v<int, CallByBoth, int>);
+  static_assert(is_invocable_with_brackets_r_v<double, CallByBoth, uint64>);
+  static_assert(is_invocable_with_brackets_r_v<uint64, CallByBoth, double>);
+
+  static_assert(!is_invocable_with_brackets_r_v<int, CallByBrackets, int, int>);
+  static_assert(!is_invocable_with_brackets_r_v<int, CallByParentheses, int>);
+  static_assert(!is_invocable_with_brackets_r_v<std::string, CallByBrackets, int>);
 }
 
 } // namespace ARIA
