b7c497e565124aa6b1abc81a65ada299990a9ba5
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-23 20:10:44 +0800

    .

--- ARIA/Core/Geometry/include/ARIA/BVH.h
+++ ARIA/Core/Geometry/include/ARIA/BVH.h
@@ -10,8 +10,10 @@
 #include "ARIA/AABB.h"
 #include "ARIA/Invocations.h"
 #include "ARIA/Launcher.h"
+#include "ARIA/MortonCode.h"
 
 #include <thrust/device_vector.h>
+#include <thrust/sort.h>
 
 namespace ARIA {
 
@@ -33,7 +35,7 @@ private:
 namespace bvh::detail {
 
 template <typename T>
-[[nodiscard]] AABB3<T> ComputeAABB(const thrust::device_vector<Vec3<T>> &positions) {
+[[nodiscard]] AABB3<T> ComputeAABB(const thrust::device_vector<Vec3<T>> &positionsD) {
   auto opVecMin = [] ARIA_HOST_DEVICE(const Vec3<T> &v0, const Vec3<T> &v1) -> Vec3<T> {
     return {min(v0.x(), v1.x()), min(v0.y(), v1.y()), min(v0.z(), v1.z())};
   };
@@ -42,11 +44,45 @@ template <typename T>
   };
 
   Vec3<T> inf =
-      thrust::reduce(positions.begin(), positions.end(), Vec3<T>{infinity<T>, infinity<T>, infinity<T>}, opVecMin);
+      thrust::reduce(positionsD.begin(), positionsD.end(), Vec3<T>{infinity<T>, infinity<T>, infinity<T>}, opVecMin);
   Vec3<T> sup =
-      thrust::reduce(positions.begin(), positions.end(), Vec3<T>{-infinity<T>, -infinity<T>, -infinity<T>}, opVecMax);
+      thrust::reduce(positionsD.begin(), positionsD.end(), Vec3<T>{-infinity<T>, -infinity<T>, -infinity<T>}, opVecMax);
 
   return AABB3<T>{inf, sup};
+}
+
+template <typename T>
+void SortByMortonCodes(const thrust::device_vector<Vec3<T>> &positionsD,
+                       const AABB3<T> &aabb,
+                       thrust::device_vector<uint> &sortedIndicesD,
+                       thrust::device_vector<uint64> &sortedMortonCodesD) {
+  // Check safety.
+  auto nPositions = Auto(positionsD.size());
+
+  if ((sortedIndicesD.size() != 0 && sortedIndicesD.size() != nPositions) ||
+      (sortedMortonCodesD.size() != 0 && sortedMortonCodesD.size() != nPositions))
+    ARIA_THROW(std::runtime_error, "Inconsistent input");
+
+  // Allocate and init.
+  if (sortedIndicesD.empty()) { // Init to [0, 1, 2, ... ] if indices have not been initialized.
+    sortedIndicesD.resize(nPositions);
+    Launcher(nPositions, [sortedIndices = sortedIndicesD.data()] ARIA_DEVICE(size_t i) {
+      sortedIndices[i] = i;
+    }).Launch();
+  }
+
+  sortedMortonCodesD.resize(nPositions);
+
+  // Compute Morton codes.
+  Launcher(nPositions,
+           [=, positions = positionsD.data(), sortedMortonCodes = sortedMortonCodesD.data()] ARIA_DEVICE(size_t i) {
+    Vec3<T> offset = aabb.offset(positions[i]);
+    Vec3u coord = (offset * T{1 << 21}).template cast<uint>(); // TODO: Magic number.
+    sortedMortonCodes[i] = MortonCode<3>::Encode(coord);
+  });
+
+  // Sort indices by Morton codes.
+  thrust::sort_by_key(sortedMortonCodesD.begin(), sortedMortonCodesD.end(), sortedIndicesD.begin());
 }
 
 } // namespace bvh::detail
@@ -57,13 +93,13 @@ template <typename T>
 template <typename TPrimitives, typename FPrimitiveToPos, typename FPrimitiveToAABB>
 [[nodiscard]] bool
 make_bvh_device(TPrimitives &&primitives, FPrimitiveToPos &&fPrimitiveToPos, FPrimitiveToAABB &&fPrimitiveToAABB) {
-  //! Check overflow.
+  // Check overflow.
   if (static_cast<uint64>(primitives.size()) > 0x7FFFFFFFLLU)
     ARIA_THROW(
         std::runtime_error,
         "Number of primitives given to the BVH excesses 0x7FFFFFFF, which can not be handled by Karras's algorithm");
 
-  //! Check and determine types.
+  // Check and determine types.
   using UPrimitivePos = decltype(invoke_with_parentheses_or_brackets(
       fPrimitiveToPos, invoke_with_parentheses_or_brackets(primitives, 0)));
   using UPrimitiveAABB = decltype(invoke_with_parentheses_or_brackets(
@@ -84,18 +120,23 @@ make_bvh_device(TPrimitives &&primitives, FPrimitiveToPos &&fPrimitiveToPos, FPr
   using TVec = UPrimitivePos;
   using TAABB = UPrimitiveAABB;
 
-  //! Map primitives to positions.
+  // Map primitives to positions.
   uint nPrimitives = primitives.size();
-  thrust::device_vector<TVec> positions(nPrimitives);
+  thrust::device_vector<TVec> positionsD(nPrimitives);
 
-  Launcher(nPrimitives, [=, ps = positions.data()] ARIA_DEVICE(uint i) {
-    // Pseudocode: `ps[i] = fPrimitiveToPos[primitives[i]];`.
-    ps[i] = invoke_with_parentheses_or_brackets(fPrimitiveToPos, invoke_with_parentheses_or_brackets(primitives, i));
+  Launcher(nPrimitives, [=, positions = positionsD.data()] ARIA_DEVICE(uint i) {
+    // Pseudocode: `positions[i] = fPrimitiveToPos[primitives[i]];`.
+    positions[i] =
+        invoke_with_parentheses_or_brackets(fPrimitiveToPos, invoke_with_parentheses_or_brackets(primitives, i));
   }).Launch();
 
-  TAABB positionsAABB = bvh::detail::ComputeAABB(positions);
+  TAABB positionsAABB = bvh::detail::ComputeAABB(positionsD);
 
-  // TODO: Compute Morton codes, sort by Morton codes, and create the reordering indices.
+  // Compute Morton codes, sort by Morton codes, and create the reordering indices.
+  thrust::device_vector<uint> sortedIndicesD;
+  thrust::device_vector<uint64> sortedMortonCodesD;
+  bvh::detail::SortByMortonCodes(positionsD, positionsAABB, sortedIndicesD, sortedMortonCodesD);
+
   //! Never explicitly reorder the primitives.
 
   // TODO: Add abstractions for CUDA streams.
