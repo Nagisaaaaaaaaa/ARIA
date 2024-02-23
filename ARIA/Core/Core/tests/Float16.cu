35c3038b01f658c366e276265784ba0dbdef675f
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-23 23:37:56 +0800

    .

--- ARIA/Core/Core/tests/Float16.cu
+++ ARIA/Core/Core/tests/Float16.cu
@@ -1,14 +1,41 @@
 #include "ARIA/Float16.h"
 
+#include <cuda/api.hpp>
 #include <gtest/gtest.h>
 
 namespace ARIA {
+
+namespace {
+
+ARIA_KERNEL void TestCUDAKernel() {
+  float16 a{0.1F};
+  float16 b{0.2F};
+  float16 c = a + b;
+  ARIA_ASSERT(std::abs(static_cast<float>(c) - 0.3F) < 0.00025F);
+
+  ARIA_ASSERT(static_cast<float>(std::numeric_limits<float16>::infinity()) == std::numeric_limits<float>::infinity());
+  ARIA_ASSERT(static_cast<float>(-std::numeric_limits<float16>::infinity()) == -std::numeric_limits<float>::infinity());
+
+  ARIA_ASSERT(static_cast<float>(cuda::std::numeric_limits<float16>::infinity()) ==
+              cuda::std::numeric_limits<float>::infinity());
+  ARIA_ASSERT(static_cast<float>(-cuda::std::numeric_limits<float16>::infinity()) ==
+              -cuda::std::numeric_limits<float>::infinity());
+}
+
+void TestCUDA() {
+  try {
+    TestCUDAKernel<<<1, 1>>>();
+    cuda::device::current::get().synchronize();
+  } catch (...) { EXPECT_FALSE(true); }
+}
+
+} // namespace
 
 TEST(Float16, Base) {
   float16 a{0.1F};
   float16 b{0.2F};
   float16 c = a + b;
-  EXPECT_TRUE(std::abs(static_cast<float>(c) - 0.3F) < 0.0001F);
+  EXPECT_TRUE(std::abs(static_cast<float>(c) - 0.3F) < 0.00025F);
 
   EXPECT_TRUE(static_cast<float>(std::numeric_limits<float16>::infinity()) == std::numeric_limits<float>::infinity());
   EXPECT_TRUE(static_cast<float>(-std::numeric_limits<float16>::infinity()) == -std::numeric_limits<float>::infinity());
@@ -17,6 +44,8 @@ TEST(Float16, Base) {
               cuda::std::numeric_limits<float>::infinity());
   EXPECT_TRUE(static_cast<float>(-cuda::std::numeric_limits<float16>::infinity()) ==
               -cuda::std::numeric_limits<float>::infinity());
+
+  TestCUDA();
 }
 
 } // namespace ARIA
