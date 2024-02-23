4bf1a29652dac66359c377429da9f3bd6deacd95
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-23 16:20:22 +0800

    adding bvh

--- ARIA/Core/Geometry/include/ARIA/BVH.h
+++ ARIA/Core/Geometry/include/ARIA/BVH.h
@@ -7,7 +7,10 @@
 //
 //
 //
-#include "ARIA/ARIA.h"
+#include "ARIA/AABB.h"
+#include "ARIA/Invocations.h"
+
+#include <thrust/device_vector.h>
 
 namespace ARIA {
 
@@ -28,11 +31,36 @@ private:
 template <typename TPrimitives, typename FPrimitiveToPos, typename FPrimitiveToAABB>
 [[nodiscard]] auto
 make_bvh_device(TPrimitives &&primitives, FPrimitiveToPos &&fPrimitiveToPos, FPrimitiveToAABB &&fPrimitiveToAABB) {
-  //! 1. Check safety.
+  //! Check safety.
   if (primitives.size() > 0x7FFFFFFFU)
     ARIA_THROW(
         std::runtime_error,
         "Number of primitives given to the BVH excesses 0x7FFFFFFF, which can not be handled by Karras's algorithm");
+
+  //! Determine types.
+  using UPrimitivePos = decltype(fPrimitiveToPos(invoke_with_parentheses_or_brackets(primitives, 0)));
+  using UPrimitiveAABB = decltype(fPrimitiveToAABB(invoke_with_parentheses_or_brackets(primitives, 0)));
+
+  static_assert(vec::detail::is_vec_v<UPrimitivePos>,
+                "Type of invocation results of `FPrimitiveToPos` should be `Vec`");
+  static_assert(aabb::detail::is_aabb_v<UPrimitiveAABB>,
+                "Type of invocation results of `FPrimitiveToAABB` should be `AABB`");
+
+  using UPrimitivePosValueType = std::decay_t<decltype(std::declval<UPrimitivePos>()[0])>;
+  using UPrimitiveAABBValueType = std::decay_t<decltype(std::declval<UPrimitiveAABB>().inf()[0])>;
+
+  static_assert(std::is_same_v<UPrimitivePosValueType, UPrimitiveAABBValueType>,
+                "Non-consistent types of `Vec` and `AABB`");
+
+  using TReal = UPrimitivePosValueType;
+  using TVec = UPrimitivePos;
+  using TAABB = UPrimitiveAABB;
+
+  //! Fetch basic information.
+  uint nPrimitives = primitives.size();
+
+  //! Map primitives to positions.
+  thrust::device_vector<TReal> positions(nPrimitives);
 
   // TODO: Compute positions of every primitives.
   // TODO: Compute AABB of all the positions.
