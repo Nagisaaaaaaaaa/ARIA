ccb1dcfb89f15064eb15ccc7850076b0cd6e58a6
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-23 15:59:02 +0800

    adding bvh

--- ARIA/Core/Geometry/include/ARIA/BVH.h
+++ ARIA/Core/Geometry/include/ARIA/BVH.h
@@ -9,4 +9,35 @@
 //
 #include "ARIA/ARIA.h"
 
-namespace ARIA {}
+namespace ARIA {
+
+template <typename TSpace, typename TPrimitives>
+class BVH;
+
+template <typename TPrimitives>
+class BVH<SpaceDevice, TPrimitives> {
+public:
+private:
+};
+
+//
+//
+//
+//
+//
+template <typename TPrimitives, typename FPrimitiveToPos, typename FPrimitiveToAABB>
+[[nodiscard]] auto
+make_bvh_device(TPrimitives &&primitives, FPrimitiveToPos &&fPrimitiveToPos, FPrimitiveToAABB &&fPrimitiveToAABB) {
+  //! 1. Check safety.
+  if (primitives.size() > 0x7FFFFFFFU)
+    ARIA_THROW(
+        std::runtime_error,
+        "Number of primitives given to the BVH excesses 0x7FFFFFFF, which can not be handled by Karras's algorithm");
+
+  // TODO: Compute positions of every primitives.
+  // TODO: Compute AABB of all the positions.
+  // TODO: Compute Morton codes, sort by Morton codes, and create the reordering indices.
+  //! Never explicitly reorder the primitives.
+}
+
+} // namespace ARIA
