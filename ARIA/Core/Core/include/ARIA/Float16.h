a6afd8e6cd16a4fc713844e3b7ab2aaf380136e7
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-23 23:22:01 +0800

    .

--- ARIA/Core/Core/include/ARIA/Float16.h
+++ ARIA/Core/Core/include/ARIA/Float16.h
@@ -7,6 +7,17 @@
 
 namespace ARIA {
 
+/// \brief A NVIDIA-based 16-bit half-precision floating point (FP16) implementation:
+/// 1 sign bit, 5 exponent bits, and 10 mantissa bits.
+///
+/// \example ```cpp
+/// float16 a{0.1F};
+/// float16 b{0.2F};
+/// float16 c = a + b;
+///
+/// float16 inf0 = std::numeric_limits<float16>::infinity();
+/// float16 inf1 = cuda::std::numeric_limits<float16>::infinity();
+/// ```
 using float16 = half;
 
 //
@@ -14,6 +25,7 @@ using float16 = half;
 //
 //
 //
+// Specialized numeric limits.
 namespace float16_::detail {
 
 struct aria_numeric_limits_float16_base_default {
@@ -98,41 +110,9 @@ public:
 
 } // namespace std
 
-//
-//
-//
 namespace cuda::std {
 
 template <>
-class numeric_limits<ARIA::float16> : public ARIA::float16_::detail::aria_numeric_limits_float16_base {
-public:
-  [[nodiscard]] static /*constexpr*/ ARIA::float16 min() noexcept = delete;
-
-  [[nodiscard]] static /*constexpr*/ ARIA::float16 max() noexcept = delete;
-
-  [[nodiscard]] static /*constexpr*/ ARIA::float16 lowest() noexcept = delete;
-
-  [[nodiscard]] static /*constexpr*/ ARIA::float16 epsilon() noexcept = delete;
-
-  [[nodiscard]] static /*constexpr*/ ARIA::float16 round_error() noexcept { return ARIA::float16{0.5F}; }
-
-  [[nodiscard]] static /*constexpr*/ ARIA::float16 denorm_min() noexcept = delete;
-
-  [[nodiscard]] static /*constexpr*/ ARIA::float16 infinity() noexcept {
-    return ARIA::float16{std::numeric_limits<float>::infinity()};
-  }
-
-  [[nodiscard]] static /*constexpr*/ ARIA::float16 quiet_NaN() noexcept = delete;
-
-  [[nodiscard]] static /*constexpr*/ ARIA::float16 signaling_NaN() noexcept = delete;
-
-  static constexpr int digits = 10;
-  // static constexpr int digits10 = ...;
-  // static constexpr int max_digits10 = ...;
-  static constexpr int max_exponent = 16;
-  // static constexpr int max_exponent10 = ...;
-  static constexpr int min_exponent = -13;
-  // static constexpr int min_exponent10 = ...;
-};
+class numeric_limits<ARIA::float16> : public ::std::numeric_limits<ARIA::float16> {};
 
 } // namespace cuda::std
--- ARIA/Core/Core/tests/Float16.cu
+++ ARIA/Core/Core/tests/Float16.cu
@@ -5,6 +5,11 @@
 namespace ARIA {
 
 TEST(Float16, Base) {
+  float16 a{0.1F};
+  float16 b{0.2F};
+  float16 c = a + b;
+  EXPECT_TRUE(std::abs(static_cast<float>(c) - 0.3F) < 0.0001F);
+
   EXPECT_TRUE(static_cast<float>(std::numeric_limits<float16>::infinity()) == std::numeric_limits<float>::infinity());
   EXPECT_TRUE(static_cast<float>(-std::numeric_limits<float16>::infinity()) == -std::numeric_limits<float>::infinity());
 
