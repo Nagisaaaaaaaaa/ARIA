e5a636b554e689ce240eabaf8b6c9e47ceb3bdcb
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-02-24 12:36:29 +0800

    .

--- ARIA/Core/Core/include/ARIA/BFloat16.h
+++ ARIA/Core/Core/include/ARIA/BFloat16.h
@@ -32,102 +32,11 @@ namespace ARIA {
 /// ```
 using bfloat16 = nv_bfloat16;
 
-//
-//
-//
-//
-//
-// Specialized numeric limits.
-namespace bfloat16_::detail {
-
-struct numeric_limits_bfloat16_base_default {
-  static constexpr std::float_denorm_style has_denorm = std::denorm_absent;
-  static constexpr bool has_denorm_loss = false;
-  static constexpr bool has_infinity = false;
-  static constexpr bool has_quiet_NaN = false;
-  static constexpr bool has_signaling_NaN = false;
-  static constexpr bool is_bounded = false;
-  static constexpr bool is_exact = false;
-  static constexpr bool is_iec559 = false;
-  static constexpr bool is_integer = false;
-  static constexpr bool is_modulo = false;
-  static constexpr bool is_signed = false;
-  static constexpr bool is_specialized = false;
-  static constexpr bool tinyness_before = false;
-  static constexpr bool traps = false;
-  static constexpr std::float_round_style round_style = std::round_toward_zero;
-  static constexpr int digits = 0;
-  static constexpr int digits10 = 0;
-  static constexpr int max_digits10 = 0;
-  static constexpr int max_exponent = 0;
-  static constexpr int max_exponent10 = 0;
-  static constexpr int min_exponent = 0;
-  static constexpr int min_exponent10 = 0;
-  static constexpr int radix = 0;
-};
-
-struct numeric_limits_bfloat16_base : numeric_limits_bfloat16_base_default {
-  static constexpr std::float_denorm_style has_denorm = std::denorm_present;
-  static constexpr bool has_infinity = true;
-  static constexpr bool has_quiet_NaN = true;
-  static constexpr bool has_signaling_NaN = true;
-  static constexpr bool is_bounded = true;
-  static constexpr bool is_iec559 = true;
-  static constexpr bool is_signed = true;
-  static constexpr bool is_specialized = true;
-  static constexpr std::float_round_style round_style = std::round_to_nearest;
-  static constexpr int radix = 2;
-};
-
-} // namespace bfloat16_::detail
-
 } // namespace ARIA
 
 //
 //
 //
-namespace std {
-
-// TODO: Implement all the functions below (even though they are seldom used).
-template <>
-class numeric_limits<ARIA::bfloat16> : public ARIA::bfloat16_::detail::numeric_limits_bfloat16_base {
-public:
-  [[nodiscard]] ARIA_HOST_DEVICE static /*constexpr*/ ARIA::bfloat16 min() noexcept = delete;
-
-  [[nodiscard]] ARIA_HOST_DEVICE static /*constexpr*/ ARIA::bfloat16 max() noexcept = delete;
-
-  [[nodiscard]] ARIA_HOST_DEVICE static /*constexpr*/ ARIA::bfloat16 lowest() noexcept = delete;
-
-  [[nodiscard]] ARIA_HOST_DEVICE static /*constexpr*/ ARIA::bfloat16 epsilon() noexcept = delete;
-
-  [[nodiscard]] ARIA_HOST_DEVICE static /*constexpr*/ ARIA::bfloat16 round_error() noexcept {
-    return ARIA::bfloat16{0.5F};
-  }
-
-  [[nodiscard]] ARIA_HOST_DEVICE static /*constexpr*/ ARIA::bfloat16 denorm_min() noexcept = delete;
-
-  [[nodiscard]] ARIA_HOST_DEVICE static /*constexpr*/ ARIA::bfloat16 infinity() noexcept {
-    return ARIA::bfloat16{std::numeric_limits<float>::infinity()};
-  }
-
-  [[nodiscard]] ARIA_HOST_DEVICE static /*constexpr*/ ARIA::bfloat16 quiet_NaN() noexcept = delete;
-
-  [[nodiscard]] ARIA_HOST_DEVICE static /*constexpr*/ ARIA::bfloat16 signaling_NaN() noexcept = delete;
-
-  static constexpr int digits = 7;
-  // static constexpr int digits10 = ...;
-  // static constexpr int max_digits10 = ...;
-  static constexpr int max_exponent = 128;
-  // static constexpr int max_exponent10 = ...;
-  static constexpr int min_exponent = -125;
-  // static constexpr int min_exponent10 = ...;
-};
-
-} // namespace std
-
-namespace cuda::std {
-
-template <>
-class numeric_limits<ARIA::bfloat16> : public ::std::numeric_limits<ARIA::bfloat16> {};
-
-} // namespace cuda::std
+//
+//
+#include "ARIA/detail/BFloat16.inc"
