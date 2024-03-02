05e463fd644875a014ab06899ca32f679a673b3d
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-03-03 02:16:10 +0800

    .

--- ARIA/Core/Core/tests/Lua.cpp
+++ ARIA/Core/Core/tests/Lua.cpp
@@ -55,6 +55,13 @@ public:
 
   void dummy2(int v) const {}
 
+  std::vector<int> dummy3(std::string v0, const std::vector<int> &v1) { return v1; }
+
+  std::vector<int> dummy3(std::string v0, const std::vector<int> &v1) const {
+    ARIA_THROW(std::runtime_error, "This method should never be called");
+    return v1;
+  }
+
 private:
   std::string name_ = "Lua です喵"; // Test UTF-8.
 
@@ -68,11 +75,16 @@ private:
 //
 #define __ARIA_LUA_NEW_USER_TYPE_BEGIN(LUA, TYPE) LUA.new_usertype<TYPE>(#TYPE
 
-#define __ARIA_LUA_NEW_USER_TYPE_METHOD_PARAMS2(TYPE, NAME)                                                            \
-  , #NAME, static_cast<decltype(std::declval<TYPE>().NAME()) (TYPE::*)()>(&TYPE::NAME)
+#define __ARIA_LUA_NEW_USER_TYPE_METHOD_PARAMS3(CONST_OR_EMPTY, TYPE, NAME)                                            \
+  , #NAME, static_cast<decltype(std::declval<TYPE>().NAME()) (TYPE::*)() CONST_OR_EMPTY>(&TYPE::NAME)
 
-#define __ARIA_LUA_NEW_USER_TYPE_METHOD_PARAMS3(TYPE, NAME, SPECIFIERS)                                                \
-  , #NAME, static_cast<decltype(std::declval<TYPE>().NAME()) (TYPE::*)() SPECIFIERS>(&TYPE::NAME)
+#define __ARIA_LUA_NEW_USER_TYPE_METHOD_PARAMS4(CONST_OR_EMPTY, TYPE, NAME, T0)                                        \
+  , #NAME, static_cast<decltype(std::declval<TYPE>().NAME(std::declval<T0>())) (TYPE::*)(T0)CONST_OR_EMPTY>(&TYPE::NAME)
+
+#define __ARIA_LUA_NEW_USER_TYPE_METHOD_PARAMS5(CONST_OR_EMPTY, TYPE, NAME, T0, T1)                                    \
+  , #NAME,                                                                                                             \
+      static_cast<decltype(std::declval<TYPE>().NAME(std::declval<T0>(), std::declval<T1>())) (TYPE::*)(               \
+          T0, T1)CONST_OR_EMPTY>(&TYPE::NAME)
 
 #define __ARIA_LUA_NEW_USER_TYPE_METHOD(...)                                                                           \
   __ARIA_EXPAND(                                                                                                       \
@@ -121,37 +133,43 @@ TEST(Lua, OwnershipAndInheritance) {
              "assert(owningChild:value() == 2, \"Should equals to 2\")");
 }
 
-TEST(Lua, Property) {
+TEST(Lua, MethodsAndProperties) {
   sol::state lua;
   lua.open_libraries(sol::lib::base);
 
+  static_assert(std::is_same_v<int &, decltype(std::declval<int &>())>);
+
   ARIA_LUA_NEW_USER_TYPE_BEGIN(lua, Object)
-  ARIA_LUA_NEW_USER_TYPE_METHOD(Object, name0, const)
-  ARIA_LUA_NEW_USER_TYPE_METHOD(Object, name1)
-  ARIA_LUA_NEW_USER_TYPE_METHOD(Object, oneTwoThree, const)
-  ARIA_LUA_NEW_USER_TYPE_METHOD(Object, dummy0)
-  ARIA_LUA_NEW_USER_TYPE_METHOD(Object, dummy1, const)
+  ARIA_LUA_NEW_USER_TYPE_METHOD(const, Object, name0)
+  ARIA_LUA_NEW_USER_TYPE_METHOD(, Object, name1)
+  ARIA_LUA_NEW_USER_TYPE_METHOD(const, Object, oneTwoThree)
+  ARIA_LUA_NEW_USER_TYPE_METHOD(, Object, dummy0)
+  ARIA_LUA_NEW_USER_TYPE_METHOD(const, Object, dummy1)
+  ARIA_LUA_NEW_USER_TYPE_METHOD(const, Object, dummy2, int)
+  ARIA_LUA_NEW_USER_TYPE_METHOD(, Object, dummy3, std::string, const std::vector<int> &)
   ARIA_LUA_NEW_USER_TYPE_END;
 
   ARIA_LUA_NEW_USER_TYPE_BEGIN(lua, decltype(std::declval<Object>().name1()))
-  ARIA_LUA_NEW_USER_TYPE_METHOD(decltype(std::declval<Object>().name1()), value)
+  ARIA_LUA_NEW_USER_TYPE_METHOD(, decltype(std::declval<Object>().name1()), value)
   ARIA_LUA_NEW_USER_TYPE_END;
 
   Object obj;
   lua["obj"] = &obj;
 
-  lua.script("local name0 = obj:name0()"
-             "local name1 = obj:name1()"
-             ""
-             "assert(name0 == \"Lua です喵\", \"Error message です喵\")"
-             "assert(tostring(name1) == \"Lua です喵\", \"Error message です喵\")"
-             "assert(name1:value() == \"Lua です喵\", \"Error message です喵\")"
-             "assert(obj:oneTwoThree()[1] == 1)"
-             "assert(obj:oneTwoThree()[2] == 2)"
-             "assert(obj:oneTwoThree()[3] == 3)"
-             "obj:dummy0()"
-             "obj:dummy1()"
-             "obj:dummy2(1)");
+  lua.script("local name0 = obj:name0()\n"
+             "local name1 = obj:name1()\n"
+             "\n"
+             "assert(name0 == \"Lua です喵\", \"Error message です喵\")\n"
+             "assert(tostring(name1) == \"Lua です喵\", \"Error message です喵\")\n"
+             "assert(name1:value() == \"Lua です喵\", \"Error message です喵\")\n"
+             "assert(obj:oneTwoThree()[1] == 1)\n"
+             "assert(obj:oneTwoThree()[2] == 2)\n"
+             "assert(obj:oneTwoThree()[3] == 3)\n"
+             "obj:dummy0()\n"
+             "obj:dummy1()\n"
+             "obj:dummy2(1)\n"
+             "obj:dummy3(obj:name0(), obj:oneTwoThree())\n"
+             "obj:dummy3(\"Lua です喵\", obj:oneTwoThree())\n");
 }
 
 } // namespace ARIA
