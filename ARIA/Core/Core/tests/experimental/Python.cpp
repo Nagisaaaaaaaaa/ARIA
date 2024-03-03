7a25c1d1490e49733ccb82fd3aa6f5a0e02fc087
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-03-03 22:50:21 +0800

    .

--- ARIA/Core/Core/tests/experimental/Python.cpp
+++ ARIA/Core/Core/tests/experimental/Python.cpp
@@ -7,29 +7,49 @@ namespace py = pybind11;
 
 namespace ARIA {
 
-namespace {
-
-int add(int a, int b) {
-  return a + b;
-}
-
-PYBIND11_EMBEDDED_MODULE(test, m) {
-  m.def("add", [](int i, int j) { return i + j; });
-}
-
-} // namespace
+namespace {} // namespace
 
 TEST(Python, Base) {
   py::scoped_interpreter guard{};
 
-  auto test = py::module_::import("test");
+  // Get scope.
+  py::object main = py::module_::import("__main__");
+  py::dict locals;
 
-  py::exec("import test\n"
-           "\n"
-           "a = 5\n"
-           "b = 6\n"
-           "c = test.add(a, b)\n"
-           "print(c)\n");
+  // Define types.
+  py::class_<std::vector<int>>(main, "vector");
+
+  // Define functions.
+  locals["add"] = py::cpp_function([](const std::vector<int> &a, std::vector<int> &b) {
+    size_t size = a.size();
+    ARIA_ASSERT(size == b.size());
+
+    std::vector<int> c(size);
+    for (size_t i = 0; i < size; ++i)
+      c[i] = a[i] + b[i];
+
+    return c;
+  });
+
+  // Define variables.
+  std::vector<int> a = {1, 2, 3};
+  std::vector<int> b = {4, 6, 9};
+
+  locals["a"] = py::cast(a, py::return_value_policy::reference);
+  locals["b"] = py::cast(b, py::return_value_policy::reference);
+
+  //
+  //
+  //
+  // Execute.
+  py::exec("c = add(a, b)\n"
+           "\n",
+           py::globals(), locals);
+
+  auto c = locals["c"].cast<std::vector<int>>();
+  for (const auto &v : c) {
+    std::cout << v << std::endl;
+  }
 }
 
 } // namespace ARIA
