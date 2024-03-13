ef1067de8e023fc902f25b74844591bfe39d23f4
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-03-13 16:51:36 +0800

    .

--- ARIA/Core/Core/include/ARIA/BitVector.h
+++ ARIA/Core/Core/include/ARIA/BitVector.h
@@ -26,17 +26,20 @@ private:
   ARIA_PROP(private, private, , bool, operatorBrackets, size_t);
 
 public:
-  explicit BitVector(size_t n = 0) : blocks_((n + nBitsPerBlock - 1) / nBitsPerBlock) {}
+  explicit BitVector(size_t n = 0) : blocks_((n + nBitsPerBlock - 1) / nBitsPerBlock), nBits_(n) {}
 
   auto operator[](size_t i) const { return operatorBrackets(i); }
 
   auto operator[](size_t i) { return operatorBrackets(i); }
+
+  size_t size() const { return nBits_; }
 
 private:
   using TBlock = uint;
   static constexpr uint nBitsPerBlock = sizeof(TBlock) * 8;
 
   std::vector<TBlock> blocks_;
+  size_t nBits_;
 
   static std::pair<size_t, uint> i2iBlocksAndiBits(size_t i) {
     size_t iBlocks = i / nBitsPerBlock;
@@ -45,27 +48,29 @@ private:
   }
 
   static bool GetBit(const TBlock &block, uint iBits) {
-    ARIA_ASSERT(iBits < nBitsPerBlock, "The given bit index should be smaller than the number of bits per block");
+    ARIA_ASSERT(iBits < nBitsPerBlock, "The given `iBits` should be smaller than the number of bits per block");
     return static_cast<bool>((block >> iBits) & TBlock{1});
   }
 
   static TBlock &FlipBit(TBlock &block, uint iBits) {
-    ARIA_ASSERT(iBits < nBitsPerBlock, "The given bit index should be smaller than the number of bits per block");
+    ARIA_ASSERT(iBits < nBitsPerBlock, "The given `iBits` should be smaller than the number of bits per block");
     return block ^= (TBlock{1} << iBits);
   }
 
   static TBlock &SetBit(TBlock &block, uint iBits, bool bit) {
-    ARIA_ASSERT(iBits < nBitsPerBlock, "The given bit index should be smaller than the number of bits per block");
+    ARIA_ASSERT(iBits < nBitsPerBlock, "The given `iBits` should be smaller than the number of bits per block");
     TBlock mask = TBlock{1} << iBits;
     return block = (block & ~mask) | (TBlock{bit} << iBits);
   }
 
   [[nodiscard]] bool ARIA_PROP_IMPL(operatorBrackets)(size_t i) const {
+    ARIA_ASSERT(i < nBits_, "The given bit index should be smaller than the total number of bits");
     auto [iBlocks, iBits] = i2iBlocksAndiBits(i);
     return GetBit(blocks_[iBlocks], iBits);
   }
 
   void ARIA_PROP_IMPL(operatorBrackets)(size_t i, bool value) {
+    ARIA_ASSERT(i < nBits_, "The given bit index should be smaller than the total number of bits");
     auto [iBlocks, iBits] = i2iBlocksAndiBits(i);
     SetBit(blocks_[iBlocks], iBits, value);
   }
--- ARIA/Core/Core/tests/BitVector.cu
+++ ARIA/Core/Core/tests/BitVector.cu
@@ -5,7 +5,18 @@
 namespace ARIA {
 
 TEST(BitVector, Base) {
-  BitVector<SpaceHost, ThreadSafe> bitVector;
+  {
+    BitVector<SpaceHost, ThreadSafe> bitVector;
+    EXPECT_EQ(bitVector.size(), 0);
+  }
+
+  for (size_t n = 0; n < 100; ++n) {
+    BitVector<SpaceHost, ThreadSafe> bitVector(n);
+    EXPECT_EQ(bitVector.size(), n);
+
+    for (size_t i = 0; i < n; ++i)
+      EXPECT_EQ(bitVector[i], false);
+  }
 }
 
 } // namespace ARIA
