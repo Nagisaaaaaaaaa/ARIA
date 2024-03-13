7399038a0fa698aec04ab89f6053281bd61c3d82
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-03-13 18:11:16 +0800

    .

--- ARIA/Core/Core/include/ARIA/BitVector.h
+++ ARIA/Core/Core/include/ARIA/BitVector.h
@@ -23,23 +23,44 @@ class BitVector<SpaceHost, ThreadUnsafe> : public std::vector<bool> {};
 template <>
 class BitVector<SpaceHost, ThreadSafe> {
 public:
-  ARIA_PROP(public, public, , bool, At, size_t);
+  explicit BitVector(size_t n = 0) { resize(n); }
+
+  ARIA_COPY_ABILITY(BitVector, default);
+
+  friend void swap(BitVector &lhs, BitVector &rhs) ARIA_NOEXCEPT {
+    using std::swap;
+    swap(lhs.blocks_, rhs.blocks_);
+    swap(lhs.nBits_, rhs.nBits_);
+  }
+
+  BitVector(BitVector &&other) ARIA_NOEXCEPT : BitVector() { swap(*this, other); }
+
+  BitVector &operator=(BitVector &&other) ARIA_NOEXCEPT {
+    swap(*this, other);
+    return *this;
+  }
 
 public:
-  explicit BitVector(size_t n = 0) : blocks_((n + nBitsPerBlock - 1) / nBitsPerBlock), nBits_(n) {}
+  ARIA_PROP(public, public, , bool, at, size_t);
 
-  auto operator[](size_t i) const { return At(i); }
+public:
+  auto operator[](size_t i) const { return at(i); }
 
-  auto operator[](size_t i) { return At(i); }
+  auto operator[](size_t i) { return at(i); }
 
   size_t size() const { return nBits_; }
+
+  void resize(size_t n) {
+    blocks_.resize((n + nBitsPerBlock - 1) / nBitsPerBlock);
+    nBits_ = n;
+  }
 
 private:
   using TBlock = uint;
   static constexpr uint nBitsPerBlock = sizeof(TBlock) * 8;
 
   std::vector<TBlock> blocks_;
-  size_t nBits_;
+  size_t nBits_ = 0;
 
   static std::pair<size_t, uint> i2iBlocksAndiBits(size_t i) {
     size_t iBlocks = i / nBitsPerBlock;
@@ -63,13 +84,13 @@ private:
     return block = (block & ~mask) | (TBlock{bit} << iBits);
   }
 
-  [[nodiscard]] bool ARIA_PROP_IMPL(At)(size_t i) const {
+  [[nodiscard]] bool ARIA_PROP_IMPL(at)(size_t i) const {
     ARIA_ASSERT(i < nBits_, "The given bit index should be smaller than the total number of bits");
     auto [iBlocks, iBits] = i2iBlocksAndiBits(i);
     return GetBit(blocks_[iBlocks], iBits);
   }
 
-  void ARIA_PROP_IMPL(At)(size_t i, bool value) {
+  void ARIA_PROP_IMPL(at)(size_t i, bool value) {
     ARIA_ASSERT(i < nBits_, "The given bit index should be smaller than the total number of bits");
     auto [iBlocks, iBits] = i2iBlocksAndiBits(i);
     SetBit(blocks_[iBlocks], iBits, value);
