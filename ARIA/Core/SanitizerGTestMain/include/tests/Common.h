873dc3c3137ee803dbd93ffe26a6fec8defc94f0
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-03-14 17:25:56 +0800

    trying sanitizer

--- ARIA/Core/SanitizerGTestMain/include/tests/Common.h
+++ ARIA/Core/SanitizerGTestMain/include/tests/Common.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#if defined(ARIA_USE_32BIT_REAL)
+  #define ASSERT_REAL_EQ(x, y) ASSERT_FLOAT_EQ(x, y)
+  #define EXPECT_REAL_EQ(x, y) EXPECT_FLOAT_EQ(x, y)
+#else
+  #define ASSERT_REAL_EQ(x, y) ASSERT_DOUBLE_EQ(x, y)
+  #define EXPECT_REAL_EQ(x, y) EXPECT_DOUBLE_EQ(x, y)
+#endif
