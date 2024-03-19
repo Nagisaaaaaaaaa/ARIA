98ec21b1c287272d95056af8d9beb959569352e3
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-03-19 14:53:01 +0800

    redesigning vdb

--- ARIA/Core/Geometry/include/ARIA/VDB.h
+++ ARIA/Core/Geometry/include/ARIA/VDB.h
@@ -7,14 +7,12 @@
 //
 //
 //
-#include "ARIA/BitVector.h"
+#include "ARIA/BitArray.h"
+#include "ARIA/ForEach.h"
 #include "ARIA/MortonCode.h"
-#include "ARIA/TensorVector.h"
 #include "ARIA/Vec.h"
 
 #include <stdgpu/unordered_map.cuh>
-
-#include <bitset>
 
 namespace ARIA {
 
@@ -35,28 +33,21 @@ public:
 
   ARIA_COPY_MOVE_ABILITY(VDBAccessor, delete, default);
 
-  // ~VDBAccessor() noexcept /* Actually, exceptions may be thrown here. */ { TBlocks::destroyDeviceObject(blocks_); }
-
-public:
-  // TODO: Implement IsValueOn().
-  // TODO: Implement GetValue().
+  ~VDBAccessor() noexcept /* Actually, exceptions may be thrown here. */ { TBlocks::destroyDeviceObject(blocks_); }
 
 private:
-  // Type of the coordinate.
-  using TCoord = Vec<int, dim>;
+  template <int N>
+  static int constexpr powN(int x) {
+    static_assert(N >= 0);
 
-  // Type of the space filling curve encoder and decoder used to hash the block coord to and from the block index.
-  using TCode = MortonCode<dim>;
+    if constexpr (N > 0) {
+      return x * powN<N - 1>(x);
+    } else {
+      return 1;
+    }
+  }
 
-  // Type of the block which contains whether each cell is on or off.
-  using TOnOffBlock = BitVector<SpaceDevice, ThreadSafe>;
-
-  // Type of the block which contains the actual value of each cell.
-  using TDataBlock = thrust::device_vector<T>;
-
-  // Type of the sparse blocks tree.
-  using TBlocks = stdgpu::unordered_map<uint64, std::pair<TOnOffBlock, TDataBlock>>;
-
+public:
   // The maximum number of blocks.
   static constexpr size_t nBlocksMax = 1024; // TODO: Maybe still too small, who knows.
 
@@ -68,24 +59,81 @@ private:
   static constexpr int nCellsPerBlockDim = 512 / (1 << dim);
   static_assert(nCellsPerBlockDim > 0, "The given dimension is too large");
 
+  // Number of cells per block.
+  static constexpr int nCellsPerBlock = powN<dim>(nCellsPerBlockDim);
+
+  // Type of the coordinate.
+  using TCoord = Vec<int, dim>;
+
+  // Type of the space filling curve encoder and decoder used to hash the block coord to and from the block index.
+  using TCode = MortonCode<dim>;
+
+  // Type of the block which contains whether each cell is on or off.
+  using TOnOffBlock = BitArray<nCellsPerBlock, ThreadSafe>;
+
+  // Type of the block which contains the actual value of each cell.
+  using TDataBlock = cuda::std::array<T, nCellsPerBlock>;
+
+  // Type of the block.
+  struct TBlock {
+    TOnOffBlock onOff;
+    TDataBlock data;
+  };
+
+  class TThreadSafeBlock {
+  public:
+    TBlock *ptr = nullptr;
+
+    ARIA_HOST_DEVICE inline void wait() noexcept {
+      cuda::std::atomic_ref lockRef{lock_};
+
+      // Wait until the lock is free.
+      while (!lockRef.load(cuda::std::memory_order_acquire)) {
+#if ARIA_IS_HOST_CODE
+  #if ARIA_ICC || ARIA_MSVC
+        _mm_pause();
+  #else
+        __builtin_ia32_pause();
+  #endif
+#else
+        __nanosleep(2);
+#endif
+      }
+    }
+
+    ARIA_HOST_DEVICE inline void go() noexcept {
+
+      cuda::std::atomic_ref lockRef{lock_};
+
+      lockRef.store(0, cuda::std::memory_order_release);
+    }
+
+  private:
+    uint lock_ = 1;
+  };
+
+  // Type of the sparse blocks tree.
+  using TBlocks = stdgpu::unordered_map<uint64, TThreadSafeBlock>;
+
   TBlocks blocks_;
 
-  [[nodiscard]] ARIA_HOST_DEVICE static uint64 BlockCoord2BlockIdx(const TCoord &blockCoord) {
-    // Compute the block index.
-    uint64 idx = TCode::Encode(blockCoord.template cast<Vec<uint64, dim>>());
-
-    // Encode the quadrant to the highest bits of the index.
-    std::bitset<64> quadrant;
+  [[nodiscard]] ARIA_HOST_DEVICE static uint64 BlockCoord2BlockIdx(TCoord blockCoord) {
+    // Compute the quadrant.
+    uint64 quadrant = 0;
     ForEach<dim>([&]<auto id>() {
       int &axis = blockCoord[id];
 
       if (axis < 0) {
         axis = -axis;
-        quadrant.set(id, true);
+        quadrant |= (1 << id); // Fill the `id`^th bit.
       }
     });
 
-    ARIA_ASSERT((idx & (std::bitset<64>().flip() << (64 - dim))) == 0,
+    // Compute the block index.
+    uint64 idx = TCode::Encode(Auto(blockCoord.template cast<uint64>()));
+
+    // Encode the quadrant to the highest bits of the index.
+    ARIA_ASSERT((idx & ((~uint64{0}) << (64 - dim))) == 0,
                 "The given block coord excesses the representation of the encoder, "
                 "please use a larger encoder instead");
     idx |= quadrant << (64 - dim);
@@ -100,6 +148,27 @@ private:
   [[nodiscard]] ARIA_HOST_DEVICE static uint64 CellCoord2BlockIdx(const TCoord &cellCoord) {
     return BlockCoord2BlockIdx(CellCoord2BlockCoord(cellCoord));
   }
+
+public:
+  ARIA_HOST_DEVICE void Allocate(const TCoord &cellCoord) {
+    auto res = blocks_.emplace(CellCoord2BlockIdx(cellCoord), TThreadSafeBlock{});
+
+    if (res.second) {
+      TThreadSafeBlock &block = res.first->second;
+
+      block.ptr = new TBlock();
+
+      block.go();
+    } else {
+      TThreadSafeBlock &block = blocks_.find(CellCoord2BlockIdx(cellCoord))->second;
+
+      block.wait();
+
+      // TODO: Do something.
+    }
+  }
+
+  ARIA_HOST_DEVICE void SetValue(const TCoord &cellCoord, const T &value) {}
 };
 
 //
