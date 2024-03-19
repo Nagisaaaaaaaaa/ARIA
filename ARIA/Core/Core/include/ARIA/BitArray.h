1c059647b41fa4be009a1f8b0719b2e6099f9591
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-03-19 13:44:45 +0800

    .

--- ARIA/Core/Core/include/ARIA/BitArray.h
+++ ARIA/Core/Core/include/ARIA/BitArray.h
@@ -1,5 +1,272 @@
 #pragma once
 
-#include "ARIA/ARIA.h"
+#include "ARIA/Property.h"
 
-namespace ARIA {}
+#include <cuda/std/array>
+#include <cuda/std/atomic>
+
+namespace ARIA {
+
+template <size_t nBits, typename TThreadSafety>
+class BitArray {
+public:
+  ARIA_HOST_DEVICE BitArray() { storage_.fill(0); }
+
+  //! Move is forbidden because it is unable to clear the array being moved.
+  //! This will make the move semantics weird.
+  //! So, users should always use copy instead of move.
+  ARIA_COPY_MOVE_ABILITY(BitArray, default, delete);
+
+public:
+  // Implementation of `BitArray` is much simpler than `BitVector`, so,
+  // codes of `BitArray` are not explained in detail.
+  // If you are interested in the implementation details, please
+  // first read the implementation of `BitVector`, see `BitVector.h`.
+  ARIA_PROP(public, public, ARIA_HOST_DEVICE, bool, at, size_t);
+
+public:
+  /// \brief Access the i^th bit.
+  ///
+  /// \warning This method is never atomic even though the `ThreadSafe` policy is used.
+  [[nodiscard]] ARIA_HOST_DEVICE auto operator[](size_t i) const { return at(i); }
+
+  /// \brief Access the i^th bit.
+  ///
+  /// \warning This method is never atomic even though the `ThreadSafe` policy is used.
+  [[nodiscard]] ARIA_HOST_DEVICE auto operator[](size_t i) { return at(i); }
+
+  /// \brief Fill the i^th bit.
+  ///
+  /// \warning This method is atomic when the `ThreadSafe` policy is used, while
+  /// non-atomic when the `ThreadUnsafe` policy is used.
+  ARIA_HOST_DEVICE BitArray &Fill(size_t i) {
+    auto [iBlocks, iBits] = i2iBlocksAndiBits(i);
+    FillBit(storage_[iBlocks], iBits);
+    return *this;
+  }
+
+  /// \brief Clear the i^th bit.
+  ///
+  /// \warning This method is atomic when the `ThreadSafe` policy is used, while
+  /// non-atomic when the `ThreadUnsafe` policy is used.
+  ARIA_HOST_DEVICE BitArray &Clear(size_t i) {
+    auto [iBlocks, iBits] = i2iBlocksAndiBits(i);
+    ClearBit(storage_[iBlocks], iBits);
+    return *this;
+  }
+
+  /// \brief Flip the i^th bit.
+  ///
+  /// \warning This method is atomic when the `ThreadSafe` policy is used, while
+  /// non-atomic when the `ThreadUnsafe` policy is used.
+  ARIA_HOST_DEVICE BitArray &Flip(size_t i) {
+    auto [iBlocks, iBits] = i2iBlocksAndiBits(i);
+    FlipBit(storage_[iBlocks], iBits);
+    return *this;
+  }
+
+  /// \brief Get the number of bits.
+  [[nodiscard]] ARIA_HOST_DEVICE consteval size_t size() const { return nBits; }
+
+private:
+  template <typename BitArrayMaybeConst>
+  class Iterator;
+
+public:
+  [[nodiscard]] ARIA_HOST_DEVICE auto begin() const { return Iterator<const BitArray>{*this, 0}; }
+
+  [[nodiscard]] ARIA_HOST_DEVICE auto begin() { return Iterator<BitArray>{*this, 0}; }
+
+  [[nodiscard]] ARIA_HOST_DEVICE auto end() const { return Iterator<const BitArray>{*this, nBits}; }
+
+  [[nodiscard]] ARIA_HOST_DEVICE auto end() { return Iterator<BitArray>{*this, nBits}; }
+
+  [[nodiscard]] ARIA_HOST_DEVICE auto cbegin() const { return begin(); }
+
+  [[nodiscard]] ARIA_HOST_DEVICE auto cend() const { return end(); }
+
+private:
+  // A "block" is a minimum unit of storage, where atomic operations can be performed on.
+  // The binary representations of blocks are the bits we are interested in.
+  using TBlock = uint;
+
+  // Number of bits per block.
+  static constexpr uint nBitsPerBlock = sizeof(TBlock) * 8;
+
+  // Number of blocks.
+  static constexpr size_t nBlocks = (nBits + nBitsPerBlock - 1) / nBitsPerBlock;
+
+  cuda::std::array<TBlock, nBlocks> storage_;
+
+private:
+  // Definitions: `i`: bit idx, `iBlocks`: block idx, `iBits`: bit idx in block.
+  [[nodiscard]] ARIA_HOST_DEVICE std::pair<size_t, uint> i2iBlocksAndiBits(size_t i) const {
+    ARIA_ASSERT(i < nBits_, "The given bit index should be smaller than the total number of bits");
+
+    size_t iBlocks = i / nBitsPerBlock;
+    uint iBits = i % nBitsPerBlock;
+    return {iBlocks, iBits};
+  }
+
+  // Get the `iBits`^th bit in `block`.
+  [[nodiscard]] ARIA_HOST_DEVICE static bool GetBit(const TBlock &block, uint iBits) {
+    return static_cast<bool>((block >> iBits) & TBlock{1});
+  }
+
+  // Fill the `iBits`^th bit in `block`.
+  ARIA_HOST_DEVICE static void FillBit(TBlock &block, uint iBits) {
+    if constexpr (std::is_same_v<TThreadSafety, ThreadUnsafe>) {
+      block |= (TBlock{1} << iBits);
+    } else if constexpr (std::is_same_v<TThreadSafety, ThreadSafe>) {
+      cuda::atomic_ref atomicBlock{block};
+      atomicBlock.fetch_or(TBlock{1} << iBits);
+    }
+  }
+
+  // Clear the `iBits`^th bit in `block`.
+  ARIA_HOST_DEVICE static void ClearBit(TBlock &block, uint iBits) {
+    if constexpr (std::is_same_v<TThreadSafety, ThreadUnsafe>) {
+      block &= ~(TBlock{1} << iBits);
+    } else if constexpr (std::is_same_v<TThreadSafety, ThreadSafe>) {
+      cuda::atomic_ref atomicBlock{block};
+      atomicBlock.fetch_and(~(TBlock{1} << iBits));
+    }
+  }
+
+  // Flip the `iBits`^th bit in `block`.
+  ARIA_HOST_DEVICE static void FlipBit(TBlock &block, uint iBits) {
+    if constexpr (std::is_same_v<TThreadSafety, ThreadUnsafe>) {
+      block ^= (TBlock{1} << iBits);
+    } else if constexpr (std::is_same_v<TThreadSafety, ThreadSafe>) {
+      cuda::atomic_ref atomicBlock{block};
+      atomicBlock.fetch_xor(TBlock{1} << iBits);
+    }
+  }
+
+  // Set the `iBits`^th bit in `block`.
+  //! Now, you may have understand why
+  //! `at(i)` and `operator[]` cannot be trivially implemented as atomic.
+  ARIA_HOST_DEVICE static void SetBit(TBlock &block, uint iBits, bool bit) {
+    TBlock mask = TBlock{1} << iBits;
+    block = (block & ~mask) | (TBlock{bit} << iBits);
+  }
+
+  [[nodiscard]] ARIA_HOST_DEVICE bool ARIA_PROP_IMPL(at)(size_t i) const {
+    auto [iBlocks, iBits] = i2iBlocksAndiBits(i);
+    return GetBit(storage_[iBlocks], iBits);
+  }
+
+  ARIA_HOST_DEVICE void ARIA_PROP_IMPL(at)(size_t i, bool value) {
+    auto [iBlocks, iBits] = i2iBlocksAndiBits(i);
+    SetBit(storage_[iBlocks], iBits, value);
+  }
+
+private:
+  template <typename BitArrayMaybeConst>
+  class Iterator {
+  public:
+    using iterator_category = std::random_access_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = bool;
+
+  public:
+    Iterator() noexcept = default;
+
+    ARIA_HOST_DEVICE explicit Iterator(BitArrayMaybeConst &v, size_t i) noexcept : v_(&v), i_(i) {}
+
+    ARIA_COPY_MOVE_ABILITY(Iterator, default, default);
+
+  public:
+    ARIA_HOST_DEVICE auto operator*() const { return (*v_)[i_]; }
+
+    ARIA_HOST_DEVICE auto operator*() { return (*v_)[i_]; }
+
+    ARIA_HOST_DEVICE auto operator->() const noexcept { return ArrowProxy((*v_)[i_]); }
+
+    ARIA_HOST_DEVICE auto operator->() noexcept { return ArrowProxy((*v_)[i_]); }
+
+    ARIA_HOST_DEVICE friend bool operator==(const Iterator &a, const Iterator &b) noexcept {
+      return a.v_ == b.v_ && a.i_ == b.i_;
+    }
+
+    ARIA_HOST_DEVICE friend bool operator!=(const Iterator &a, const Iterator &b) noexcept { return !operator==(a, b); }
+
+    // ++it
+    ARIA_HOST_DEVICE Iterator &operator++() {
+      ++i_;
+      return *this;
+    }
+
+    // it++
+    ARIA_HOST_DEVICE Iterator operator++(int) {
+      Iterator tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    // --it
+    ARIA_HOST_DEVICE Iterator &operator--() {
+      --i_;
+      return *this;
+    }
+
+    // it--
+    ARIA_HOST_DEVICE Iterator operator--(int) {
+      Iterator tmp = *this;
+      --(*this);
+      return tmp;
+    }
+
+    ARIA_HOST_DEVICE Iterator operator+(size_t n) const {
+      Iterator temp = *this;
+      return (temp += n);
+    };
+
+    ARIA_HOST_DEVICE Iterator operator-(size_t n) const {
+      Iterator temp = *this;
+      return (temp -= n);
+    };
+
+    ARIA_HOST_DEVICE friend Iterator operator+(size_t n, const Iterator &it) { return it + n; }
+
+    ARIA_HOST_DEVICE friend Iterator operator-(size_t n, const Iterator &it) { return it - n; }
+
+    ARIA_HOST_DEVICE Iterator &operator+=(size_t n) {
+      i_ += n;
+      return *this;
+    }
+
+    ARIA_HOST_DEVICE Iterator &operator-=(size_t n) {
+      i_ -= n;
+      return *this;
+    }
+
+    ARIA_HOST_DEVICE auto operator[](size_t n) const { return (*v_)[i_ + n]; }
+
+    ARIA_HOST_DEVICE friend difference_type operator-(const Iterator &lhs, const Iterator &rhs) noexcept {
+      return lhs.i_ - rhs.i_;
+    }
+
+    ARIA_HOST_DEVICE friend bool operator<(const Iterator &lhs, const Iterator &rhs) noexcept {
+      return lhs.i_ < rhs.i_;
+    }
+
+    ARIA_HOST_DEVICE friend bool operator<=(const Iterator &lhs, const Iterator &rhs) noexcept {
+      return lhs.i_ <= rhs.i_;
+    }
+
+    ARIA_HOST_DEVICE friend bool operator>(const Iterator &lhs, const Iterator &rhs) noexcept {
+      return lhs.i_ > rhs.i_;
+    }
+
+    ARIA_HOST_DEVICE friend bool operator>=(const Iterator &lhs, const Iterator &rhs) noexcept {
+      return lhs.i_ >= rhs.i_;
+    }
+
+  private:
+    BitArrayMaybeConst *v_ = nullptr;
+    size_t i_ = 0;
+  };
+};
+
+} // namespace ARIA
--- ARIA/Core/Core/tests/BitArray.cu
+++ ARIA/Core/Core/tests/BitArray.cu
@@ -1,9 +1,250 @@
 #include "ARIA/BitArray.h"
+#include "ARIA/ForEach.h"
 
 #include <gtest/gtest.h>
 
 namespace ARIA {
 
-TEST(BitArray, Base) {}
+TEST(BitArray, Base) {
+  auto testBitArrayBase = []<typename TThreadSafety>() {
+    // Constructors.
+    {
+      BitArray<0, TThreadSafety> bitVector;
+      static_assert(bitVector.size() == 0);
+    }
+
+    ForEach<100>([]<auto n>() {
+      BitArray<n, TThreadSafety> bitVector;
+      static_assert(bitVector.size() == n);
+
+      for (size_t i = 0; i < n; ++i)
+        EXPECT_EQ(bitVector[i], false);
+    });
+
+    // Operator[].
+    ForEach<100>([]<auto n>() {
+      BitArray<n, TThreadSafety> bitVector;
+
+      for (size_t i = 0; i < n; ++i)
+        if (i % 2 == 0)
+          bitVector[i] = true;
+
+      for (size_t i = 0; i < n; ++i)
+        EXPECT_EQ(bitVector[i], i % 2 == 0 ? true : false);
+
+      for (size_t i = 0; i < n; ++i)
+        if (i % 2 == 1)
+          bitVector[i] = true;
+
+      for (size_t i = 0; i < n; ++i)
+        EXPECT_EQ(bitVector[i], true);
+
+      for (size_t i = 0; i < n; ++i)
+        if (i % 2 == 0)
+          bitVector[i] = false;
+
+      for (size_t i = 0; i < n; ++i)
+        EXPECT_EQ(bitVector[i], i % 2 == 0 ? false : true);
+
+      for (size_t i = 0; i < n; ++i)
+        if (i % 2 == 1)
+          bitVector[i] = false;
+
+      for (size_t i = 0; i < n; ++i)
+        EXPECT_EQ(bitVector[i], false);
+    });
+
+    // Copy.
+    {
+      constexpr size_t n = 10;
+      BitArray<n, TThreadSafety> bitVector0;
+      for (size_t i = 0; i < n; ++i)
+        bitVector0[i] = true;
+
+      auto bitVector1 = bitVector0;
+      EXPECT_EQ(bitVector0.size(), n);
+      EXPECT_EQ(bitVector1.size(), n);
+      for (size_t i = 0; i < n; ++i)
+        EXPECT_EQ(bitVector0[i], true);
+      for (size_t i = 0; i < n; ++i)
+        EXPECT_EQ(bitVector1[i], true);
+    }
+
+    // Non-const `begin` and `end`.
+    {
+      constexpr size_t n = 10;
+      BitArray<n, TThreadSafety> bitVector;
+
+      for (size_t i = 0; i < n; ++i)
+        if (i % 2 == 1)
+          bitVector[i] = true;
+
+      size_t i = 0;
+      for (auto it = bitVector.begin(); it != bitVector.end(); ++it, ++i) {
+        bool v = *it;
+
+        if (i % 2 == 1) {
+          EXPECT_TRUE(*it);
+          EXPECT_TRUE(v);
+          EXPECT_TRUE(it->value());
+          EXPECT_EQ(*it, true);
+          EXPECT_EQ(v, true);
+          EXPECT_EQ(it->value(), true);
+        } else {
+          EXPECT_FALSE(*it);
+          EXPECT_FALSE(v);
+          EXPECT_FALSE(it->value());
+          EXPECT_EQ(*it, false);
+          EXPECT_EQ(v, false);
+          EXPECT_EQ(it->value(), false);
+        }
+      }
+      EXPECT_EQ(i, n);
+    }
+
+    // Const `begin` and `end`.
+    {
+      constexpr size_t n = 10;
+      BitArray<n, TThreadSafety> bitVectorNonConst;
+
+      for (size_t i = 0; i < n; ++i)
+        if (i % 2 == 1)
+          bitVectorNonConst[i] = true;
+
+      const BitArray<n, TThreadSafety> bitVector = bitVectorNonConst;
+
+      size_t i = 0;
+      for (auto it = bitVector.begin(); it != bitVector.end(); ++it, ++i) {
+        bool v = *it;
+
+        if (i % 2 == 1) {
+          EXPECT_TRUE(*it);
+          EXPECT_TRUE(v);
+          EXPECT_TRUE(it->value());
+          EXPECT_EQ(*it, true);
+          EXPECT_EQ(v, true);
+          EXPECT_EQ(it->value(), true);
+        } else {
+          EXPECT_FALSE(*it);
+          EXPECT_FALSE(v);
+          EXPECT_FALSE(it->value());
+          EXPECT_EQ(*it, false);
+          EXPECT_EQ(v, false);
+          EXPECT_EQ(it->value(), false);
+        }
+      }
+      EXPECT_EQ(i, n);
+    }
+
+    // `cbegin` and `cend`.
+    {
+      constexpr size_t n = 10;
+      BitArray<n, TThreadSafety> bitVectorNonConst;
+
+      for (size_t i = 0; i < n; ++i)
+        if (i % 2 == 1)
+          bitVectorNonConst[i] = true;
+
+      const BitArray<n, TThreadSafety> bitVector = bitVectorNonConst;
+
+      size_t i = 0;
+      for (auto it = bitVector.cbegin(); it != bitVector.cend(); ++it, ++i) {
+        bool v = *it;
+
+        if (i % 2 == 1) {
+          EXPECT_TRUE(*it);
+          EXPECT_TRUE(v);
+          EXPECT_TRUE(it->value());
+          EXPECT_EQ(*it, true);
+          EXPECT_EQ(v, true);
+          EXPECT_EQ(it->value(), true);
+        } else {
+          EXPECT_FALSE(*it);
+          EXPECT_FALSE(v);
+          EXPECT_FALSE(it->value());
+          EXPECT_EQ(*it, false);
+          EXPECT_EQ(v, false);
+          EXPECT_EQ(it->value(), false);
+        }
+      }
+      EXPECT_EQ(i, n);
+    }
+
+    // Non-const range-based for.
+    {
+      constexpr size_t n = 10;
+      BitArray<n, TThreadSafety> bitVector;
+
+      for (size_t i = 0; i < n; ++i)
+        if (i % 2 == 1)
+          bitVector[i] = true;
+
+      size_t i = 0;
+      for (Property auto v : bitVector) {
+        if (i % 2 == 1) {
+          EXPECT_TRUE(v);
+          EXPECT_TRUE(v.value());
+          EXPECT_EQ(v, true);
+          EXPECT_EQ(v.value(), true);
+        } else {
+          EXPECT_FALSE(v);
+          EXPECT_FALSE(v.value());
+          EXPECT_EQ(v, false);
+          EXPECT_EQ(v.value(), false);
+        }
+        ++i;
+      }
+      EXPECT_EQ(i, n);
+    }
+
+    // Const range-based for.
+    {
+      constexpr size_t n = 10;
+      BitArray<n, TThreadSafety> bitVectorNonConst;
+
+      for (size_t i = 0; i < n; ++i)
+        if (i % 2 == 1)
+          bitVectorNonConst[i] = true;
+
+      const BitArray<n, TThreadSafety> bitVector = bitVectorNonConst;
+
+      size_t i = 0;
+      for (Property auto v : bitVector) {
+        if (i % 2 == 1) {
+          EXPECT_TRUE(v);
+          EXPECT_TRUE(v.value());
+          EXPECT_EQ(v, true);
+          EXPECT_EQ(v.value(), true);
+        } else {
+          EXPECT_FALSE(v);
+          EXPECT_FALSE(v.value());
+          EXPECT_EQ(v, false);
+          EXPECT_EQ(v.value(), false);
+        }
+        ++i;
+      }
+      EXPECT_EQ(i, n);
+    }
+
+    // Iterator requirements.
+    {
+      BitArray<10, TThreadSafety> t0;
+      const BitArray<10, TThreadSafety> t1;
+
+      auto testIteratorRequirements = [](auto v) {
+        static_assert(std::random_access_iterator<decltype(v.begin())>);
+        static_assert(std::random_access_iterator<decltype(v.end())>);
+        static_assert(std::random_access_iterator<decltype(v.cbegin())>);
+        static_assert(std::random_access_iterator<decltype(v.cend())>);
+      };
+
+      testIteratorRequirements(t0);
+      testIteratorRequirements(t1);
+    }
+  };
+
+  testBitArrayBase.operator()<ThreadUnsafe>();
+  testBitArrayBase.operator()<ThreadSafe>();
+}
 
 } // namespace ARIA
