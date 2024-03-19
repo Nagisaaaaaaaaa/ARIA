b077093e31c08616f0ec315e7210940b57e0462d
nagisaaaaaaaaa <1970354622wuyh@gmail.com>
2024-03-19 17:58:18 +0800

    .

--- ARIA/Core/Geometry/include/ARIA/VDB.h
+++ ARIA/Core/Geometry/include/ARIA/VDB.h
@@ -40,33 +40,37 @@ ARIA_HOST_DEVICE static int consteval powN(int x) {
 
 } // namespace vdb::detail
 
-// Device VDB accessor.
+// VDB handle.
 template <typename T, auto dim>
-class VDBAccessor<T, dim, SpaceDevice> {
+class VDBHandle {
 public:
-  VDBAccessor() : blocks_(TBlocks::createDeviceObject(nBlocksMax)) {}
+  ARIA_COPY_MOVE_ABILITY(VDBHandle, default, default);
 
-  ARIA_COPY_MOVE_ABILITY(VDBAccessor, delete, default);
+  [[nodiscard]] static VDBHandle Create() {
+    VDBHandle handle;
+    handle.blocks_ = TBlocks::createDeviceObject(nBlocksMax);
+    return handle;
+  }
 
-  ~VDBAccessor() noexcept /* Actually, exceptions may be thrown here. */ { TBlocks::destroyDeviceObject(blocks_); }
+  void Destroy() noexcept /* Actually, exceptions may be thrown here. */ { TBlocks::destroyDeviceObject(blocks_); }
 
   //
   //
   //
-public:
-  // The maximum number of blocks.
-  static constexpr size_t nBlocksMax = 1024; // TODO: Maybe still too small, who knows.
+private:
+  // Maximum number of blocks.
+  static constexpr size_t nBlocksMax = 512; // TODO: Maybe still too small, who knows.
 
   // Number of cells per dim of each block.
-  // Eg: dim: 1    1 << dim: 2    512 / (1 << dim): 256    #cells per block: 256
-  //          2              4                      128                      16384
-  //          3              8                      64                       262144
-  //          4              16                     32                       1048576
-  static constexpr int nCellsPerBlockDim = 512 / (1 << dim);
-  static_assert(nCellsPerBlockDim > 0, "The given dimension is too large");
+  // Eg: dim: 1    1 << dim: 2    nCellsPerBlockDim: 256    nCellsPerBlock: 256
+  //          2              4                       128                    16384
+  //          3              8                       64                     262144
+  //          4              16                      32                     1048576
+  //          5                                      32                     33554432
+  static constexpr int nCellsPerBlockDim = std::max(512 / (1 << dim), 32);
 
   // Number of cells per block.
-  static constexpr int nCellsPerBlock = vdb::detail::powN<dim>(nCellsPerBlockDim);
+  static constexpr int nCellsPerBlock = vdb::detail::powN<dim>(nCellsPerBlockDim); // = nCellsPerBlockDim^dim
 
   //
   //
@@ -74,31 +78,40 @@ public:
   // Type of the coordinate.
   using TCoord = Vec<int, dim>;
 
-  // Type of the space filling curve encoder and decoder used to hash the block coord to and from the block index.
+  // Type of the space filling curve encoder and decoder, which
+  // is used to hash the block coord to and from the block index.
   using TCode = MortonCode<dim>;
 
-  // Type of the block storage which contains whether each cell is on or off.
-  using TOnOffBlockStorage = BitArray<nCellsPerBlock, ThreadSafe>;
+  // Type of the block storage part, which contains whether each cell is on or off.
+  using TBlockStorageOnOff = BitArray<nCellsPerBlock, ThreadSafe>;
 
-  // Type of the block storage which contains the actual value of each cell.
-  using TDataBlockStroage = cuda::std::array<T, nCellsPerBlock>;
+  // Type of the block storage part, which contains the actual value of each cell.
+  using TBlockStorageData = cuda::std::array<T, nCellsPerBlock>;
 
-  // Type of the block storage which contains all things of a block.
+  // Type of the block storage.
   struct TBlockStorage {
-    TOnOffBlockStorage onOff;
-    TDataBlockStroage data;
+    TBlockStorageOnOff onOff;
+    TBlockStorageData data;
   };
 
-  // Type of the block.
+  // Type of the block, which contains the block storage pointer and a barrier.
   class TBlock {
   public:
     TBlockStorage *p = nullptr;
 
-    ARIA_HOST_DEVICE inline void wait() noexcept {
-      cuda::std::atomic_ref lockRef{lock_};
+    // A thread calls this method to mark the storage as ready.
+    ARIA_HOST_DEVICE inline void arrive() noexcept {
+      cuda::std::atomic_ref barrier{barrier_};
 
-      // Wait until the lock is free.
-      while (!lockRef.load(cuda::std::memory_order_acquire)) {
+      barrier.store(0, cuda::std::memory_order_release);
+    }
+
+    // A thread calls this method to wait for the storage being ready.
+    ARIA_HOST_DEVICE inline void wait() noexcept {
+      cuda::std::atomic_ref barrier{barrier_};
+
+      // Spin until the barrier is ready.
+      while (!barrier.load(cuda::std::memory_order_acquire)) {
 #if ARIA_IS_HOST_CODE
   #if ARIA_ICC || ARIA_MSVC
         _mm_pause();
@@ -111,21 +124,20 @@ public:
       }
     }
 
-    ARIA_HOST_DEVICE inline void go() noexcept {
-      cuda::std::atomic_ref lockRef{lock_};
-
-      lockRef.store(0, cuda::std::memory_order_release);
-    }
-
   private:
-    uint lock_ = 1;
+    uint barrier_ = 1;
   };
 
-  // Type of the sparse blocks tree.
+  // Type of the sparse blocks tree:
+  //   Key  : Code of the block coord (defined by TCode).
+  //   Value: The block.
   using TBlocks = stdgpu::unordered_map<uint64, TBlock>;
 
   TBlocks blocks_;
 
+  //
+  //
+  //
   [[nodiscard]] ARIA_HOST_DEVICE static uint64 BlockCoord2BlockIdx(TCoord blockCoord) {
     // Compute the quadrant.
     uint64 quadrant = 0;
@@ -151,7 +163,10 @@ public:
   }
 
   [[nodiscard]] ARIA_HOST_DEVICE static TCoord CellCoord2BlockCoord(const TCoord &cellCoord) {
-    constexpr auto n = nCellsPerBlockDim; // TODO: Compiler bug here.
+    // TODO: Compiler bug here: `nCellsPerBlockDim` is not defined in device code.
+    // return cellCoord / nCellsPerBlockDim;
+
+    constexpr auto n = nCellsPerBlockDim;
     return cellCoord / n;
   }
 
@@ -160,21 +175,30 @@ public:
   }
 
 public:
-  ARIA_HOST_DEVICE void Allocate(const TCoord &cellCoord) {
+  ARIA_HOST_DEVICE TBlock &Access(const TCoord &cellCoord) {
+    // Each thread is trying to insert an empty block into the unordered map,
+    // but only one unique thread will succeed.
     auto res = blocks_.emplace(CellCoord2BlockIdx(cellCoord), TBlock{});
 
-    if (res.second) {
-      TBlock &block = res.first->second;
+    // If success, get reference to the emplaced block.
+    TBlock &block = res.first->second;
 
+    if (res.second) { // For the unique thread which succeeded to emplace the block:
+      // Allocate the block storage.
       block.p = new TBlockStorage();
 
-      block.go();
-    } else {
-      TBlock &block = blocks_.find(CellCoord2BlockIdx(cellCoord))->second;
+      // Mark the storage as ready.
+      block.arrive();
+    } else { // For other threads which failed:
+      // Get reference to the emplaced block.
+      block = blocks_.find(CellCoord2BlockIdx(cellCoord))->second;
 
-      // Wait until the block is allocated.
+      // Wait for the storage being ready.
       block.wait();
     }
+
+    // For now, all threads have access to the emplaced block.
+    return block;
   }
 };
 
