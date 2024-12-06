6df195cbd4fe29a928c3a679662eef20bdf71d06
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2024-12-06 14:37:02 +0800

    .

--- ARIA/Core/Core/tests/Mosaic.cu
+++ ARIA/Core/Core/tests/Mosaic.cu
@@ -6,19 +6,19 @@ namespace ARIA {
 
 namespace {
 
-struct StructWith0Member {};
+struct Test0Member {};
 
 //! Structures with 1 member is considered unnecessary.
-struct StructWith1Member {
+struct Test1Member {
   int v = 5;
 };
 
-struct StructWith2Members {
+struct Test2Members {
   int v0 = 5;
   double v1 = 6;
 };
 
-struct StructWithPrivateMembers {
+struct TestPrivateMembers {
 private:
   int v_ = 5;
 };
@@ -31,50 +31,50 @@ TEST(Mosaic, Base) {
   static_assert(boost::pfr::tuple_size_v<int> == 1);
   static_assert(is_mosaic_v<int>);
 
-  static_assert(!std::is_scalar_v<StructWith0Member>);
-  static_assert(std::is_aggregate_v<StructWith0Member>);
-  static_assert(boost::pfr::tuple_size_v<StructWith0Member> == 0);
-  static_assert(!is_mosaic_v<StructWith0Member>);
+  static_assert(!std::is_scalar_v<Test0Member>);
+  static_assert(std::is_aggregate_v<Test0Member>);
+  static_assert(boost::pfr::tuple_size_v<Test0Member> == 0);
+  static_assert(!is_mosaic_v<Test0Member>);
 
-  static_assert(!std::is_scalar_v<StructWith1Member>);
-  static_assert(std::is_aggregate_v<StructWith1Member>);
-  // static_assert(boost::pfr::tuple_size_v<StructWith1Member> == 1);
-  // static_assert(!is_mosaic_v<StructWith1Member>);
+  static_assert(!std::is_scalar_v<Test1Member>);
+  static_assert(std::is_aggregate_v<Test1Member>);
+  // static_assert(boost::pfr::tuple_size_v<Test1Member> == 1);
+  // static_assert(!is_mosaic_v<Test1Member>);
 
-  static_assert(!std::is_scalar_v<StructWith2Members>);
-  static_assert(std::is_aggregate_v<StructWith2Members>);
-  static_assert(boost::pfr::tuple_size_v<StructWith2Members> == 2);
-  static_assert(is_mosaic_v<StructWith2Members>);
+  static_assert(!std::is_scalar_v<Test2Members>);
+  static_assert(std::is_aggregate_v<Test2Members>);
+  static_assert(boost::pfr::tuple_size_v<Test2Members> == 2);
+  static_assert(is_mosaic_v<Test2Members>);
 
-  static_assert(!std::is_scalar_v<StructWithPrivateMembers>);
-  static_assert(!std::is_aggregate_v<StructWithPrivateMembers>);
-  // static_assert(boost::pfr::tuple_size_v<StructWithPrivateMembers> == 0);
-  // static_assert(!is_mosaic_v<StructWithPrivateMembers>);
+  static_assert(!std::is_scalar_v<TestPrivateMembers>);
+  static_assert(!std::is_aggregate_v<TestPrivateMembers>);
+  // static_assert(boost::pfr::tuple_size_v<TestPrivateMembers> == 0);
+  // static_assert(!is_mosaic_v<TestPrivateMembers>);
 
   static_assert(std::is_scalar_v<double *>);
   static_assert(!std::is_aggregate_v<double *>);
   static_assert(boost::pfr::tuple_size_v<double *> == 1);
   static_assert(is_mosaic_v<double *>);
 
-  static_assert(std::is_scalar_v<StructWith0Member *>);
-  static_assert(!std::is_aggregate_v<StructWith0Member *>);
-  static_assert(boost::pfr::tuple_size_v<StructWith0Member *> == 1);
-  static_assert(is_mosaic_v<StructWith0Member *>);
+  static_assert(std::is_scalar_v<Test0Member *>);
+  static_assert(!std::is_aggregate_v<Test0Member *>);
+  static_assert(boost::pfr::tuple_size_v<Test0Member *> == 1);
+  static_assert(is_mosaic_v<Test0Member *>);
 
-  static_assert(std::is_scalar_v<StructWith1Member *>);
-  static_assert(!std::is_aggregate_v<StructWith1Member *>);
-  static_assert(boost::pfr::tuple_size_v<StructWith1Member *> == 1);
-  static_assert(is_mosaic_v<StructWith1Member *>);
+  static_assert(std::is_scalar_v<Test1Member *>);
+  static_assert(!std::is_aggregate_v<Test1Member *>);
+  static_assert(boost::pfr::tuple_size_v<Test1Member *> == 1);
+  static_assert(is_mosaic_v<Test1Member *>);
 
-  static_assert(std::is_scalar_v<StructWith2Members *>);
-  static_assert(!std::is_aggregate_v<StructWith2Members *>);
-  static_assert(boost::pfr::tuple_size_v<StructWith2Members *> == 1);
-  static_assert(is_mosaic_v<StructWith2Members *>);
+  static_assert(std::is_scalar_v<Test2Members *>);
+  static_assert(!std::is_aggregate_v<Test2Members *>);
+  static_assert(boost::pfr::tuple_size_v<Test2Members *> == 1);
+  static_assert(is_mosaic_v<Test2Members *>);
 
-  static_assert(std::is_scalar_v<StructWithPrivateMembers *>);
-  static_assert(!std::is_aggregate_v<StructWithPrivateMembers *>);
-  static_assert(boost::pfr::tuple_size_v<StructWithPrivateMembers *> == 1);
-  static_assert(is_mosaic_v<StructWithPrivateMembers *>);
+  static_assert(std::is_scalar_v<TestPrivateMembers *>);
+  static_assert(!std::is_aggregate_v<TestPrivateMembers *>);
+  static_assert(boost::pfr::tuple_size_v<TestPrivateMembers *> == 1);
+  static_assert(is_mosaic_v<TestPrivateMembers *>);
 }
 
 } // namespace ARIA
