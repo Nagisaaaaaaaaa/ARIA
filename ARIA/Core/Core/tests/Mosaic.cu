7dc332e9465e90780a8d93d04f4f5675ddb0ec9b
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2024-12-06 14:46:41 +0800

    .

--- ARIA/Core/Core/tests/Mosaic.cu
+++ ARIA/Core/Core/tests/Mosaic.cu
@@ -39,6 +39,15 @@ struct TestRecursive1Member {
   } s1;
 };
 
+struct TestRecursive2Members {
+  int v0 = 5;
+
+  struct {
+    double v1 = 6;
+    int *v2 = nullptr;
+  } s1;
+};
+
 } // namespace
 
 TEST(Mosaic, Base) {
@@ -77,6 +86,11 @@ TEST(Mosaic, Base) {
   static_assert(boost::pfr::tuple_size_v<TestRecursive1Member> == 2);
   // static_assert(!is_mosaic_v<TestRecursive1Member>);
 
+  static_assert(!std::is_scalar_v<TestRecursive2Members>);
+  static_assert(std::is_aggregate_v<TestRecursive2Members>);
+  static_assert(boost::pfr::tuple_size_v<TestRecursive2Members> == 2);
+  static_assert(is_mosaic_v<TestRecursive2Members>);
+
   static_assert(std::is_scalar_v<double *>);
   static_assert(!std::is_aggregate_v<double *>);
   static_assert(boost::pfr::tuple_size_v<double *> == 1);
@@ -111,6 +125,11 @@ TEST(Mosaic, Base) {
   static_assert(!std::is_aggregate_v<TestRecursive1Member *>);
   static_assert(boost::pfr::tuple_size_v<TestRecursive1Member *> == 1);
   static_assert(is_mosaic_v<TestRecursive1Member *>);
+
+  static_assert(std::is_scalar_v<TestRecursive2Members *>);
+  static_assert(!std::is_aggregate_v<TestRecursive2Members *>);
+  static_assert(boost::pfr::tuple_size_v<TestRecursive2Members *> == 1);
+  static_assert(is_mosaic_v<TestRecursive2Members *>);
 }
 
 } // namespace ARIA
