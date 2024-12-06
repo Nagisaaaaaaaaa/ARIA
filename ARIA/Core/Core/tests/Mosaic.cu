837f1b7c0338921b92629566a724ff29fa69e71b
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2024-12-06 14:52:17 +0800

    .

--- ARIA/Core/Core/tests/Mosaic.cu
+++ ARIA/Core/Core/tests/Mosaic.cu
@@ -51,85 +51,89 @@ struct TestRecursive2Members {
 } // namespace
 
 TEST(Mosaic, Base) {
-  static_assert(std::is_scalar_v<int>);
-  static_assert(!std::is_aggregate_v<int>);
-  static_assert(boost::pfr::tuple_size_v<int> == 1);
-  static_assert(is_mosaic_v<int>);
+  {
+    static_assert(std::is_scalar_v<int>);
+    static_assert(!std::is_aggregate_v<int>);
+    static_assert(boost::pfr::tuple_size_v<int> == 1);
+    static_assert(is_mosaic_v<int>);
 
-  static_assert(!std::is_scalar_v<Test0Member>);
-  static_assert(std::is_aggregate_v<Test0Member>);
-  static_assert(boost::pfr::tuple_size_v<Test0Member> == 0);
-  static_assert(!is_mosaic_v<Test0Member>);
+    static_assert(!std::is_scalar_v<Test0Member>);
+    static_assert(std::is_aggregate_v<Test0Member>);
+    static_assert(boost::pfr::tuple_size_v<Test0Member> == 0);
+    static_assert(!is_mosaic_v<Test0Member>);
 
-  static_assert(!std::is_scalar_v<Test1Member>);
-  static_assert(std::is_aggregate_v<Test1Member>);
-  // static_assert(boost::pfr::tuple_size_v<Test1Member> == 1);
-  // static_assert(!is_mosaic_v<Test1Member>);
+    static_assert(!std::is_scalar_v<Test1Member>);
+    static_assert(std::is_aggregate_v<Test1Member>);
+    // static_assert(boost::pfr::tuple_size_v<Test1Member> == 1);
+    // static_assert(!is_mosaic_v<Test1Member>);
 
-  static_assert(!std::is_scalar_v<Test2Members>);
-  static_assert(std::is_aggregate_v<Test2Members>);
-  static_assert(boost::pfr::tuple_size_v<Test2Members> == 2);
-  static_assert(is_mosaic_v<Test2Members>);
+    static_assert(!std::is_scalar_v<Test2Members>);
+    static_assert(std::is_aggregate_v<Test2Members>);
+    static_assert(boost::pfr::tuple_size_v<Test2Members> == 2);
+    static_assert(is_mosaic_v<Test2Members>);
 
-  static_assert(!std::is_scalar_v<TestPrivateMembers>);
-  static_assert(!std::is_aggregate_v<TestPrivateMembers>);
-  // static_assert(boost::pfr::tuple_size_v<TestPrivateMembers> == 0);
-  // static_assert(!is_mosaic_v<TestPrivateMembers>);
+    static_assert(!std::is_scalar_v<TestPrivateMembers>);
+    static_assert(!std::is_aggregate_v<TestPrivateMembers>);
+    // static_assert(boost::pfr::tuple_size_v<TestPrivateMembers> == 0);
+    // static_assert(!is_mosaic_v<TestPrivateMembers>);
 
-  static_assert(!std::is_scalar_v<TestRecursive0Member>);
-  static_assert(std::is_aggregate_v<TestRecursive0Member>);
-  static_assert(boost::pfr::tuple_size_v<TestRecursive0Member> == 2);
-  static_assert(!is_mosaic_v<TestRecursive0Member>);
+    static_assert(!std::is_scalar_v<TestRecursive0Member>);
+    static_assert(std::is_aggregate_v<TestRecursive0Member>);
+    static_assert(boost::pfr::tuple_size_v<TestRecursive0Member> == 2);
+    static_assert(!is_mosaic_v<TestRecursive0Member>);
 
-  static_assert(!std::is_scalar_v<TestRecursive1Member>);
-  static_assert(std::is_aggregate_v<TestRecursive1Member>);
-  static_assert(boost::pfr::tuple_size_v<TestRecursive1Member> == 2);
-  // static_assert(!is_mosaic_v<TestRecursive1Member>);
+    static_assert(!std::is_scalar_v<TestRecursive1Member>);
+    static_assert(std::is_aggregate_v<TestRecursive1Member>);
+    static_assert(boost::pfr::tuple_size_v<TestRecursive1Member> == 2);
+    // static_assert(!is_mosaic_v<TestRecursive1Member>);
 
-  static_assert(!std::is_scalar_v<TestRecursive2Members>);
-  static_assert(std::is_aggregate_v<TestRecursive2Members>);
-  static_assert(boost::pfr::tuple_size_v<TestRecursive2Members> == 2);
-  static_assert(is_mosaic_v<TestRecursive2Members>);
+    static_assert(!std::is_scalar_v<TestRecursive2Members>);
+    static_assert(std::is_aggregate_v<TestRecursive2Members>);
+    static_assert(boost::pfr::tuple_size_v<TestRecursive2Members> == 2);
+    static_assert(is_mosaic_v<TestRecursive2Members>);
+  }
 
-  static_assert(std::is_scalar_v<double *>);
-  static_assert(!std::is_aggregate_v<double *>);
-  static_assert(boost::pfr::tuple_size_v<double *> == 1);
-  static_assert(is_mosaic_v<double *>);
+  {
+    static_assert(std::is_scalar_v<double *>);
+    static_assert(!std::is_aggregate_v<double *>);
+    static_assert(boost::pfr::tuple_size_v<double *> == 1);
+    static_assert(is_mosaic_v<double *>);
 
-  static_assert(std::is_scalar_v<Test0Member *>);
-  static_assert(!std::is_aggregate_v<Test0Member *>);
-  static_assert(boost::pfr::tuple_size_v<Test0Member *> == 1);
-  static_assert(is_mosaic_v<Test0Member *>);
+    static_assert(std::is_scalar_v<Test0Member *>);
+    static_assert(!std::is_aggregate_v<Test0Member *>);
+    static_assert(boost::pfr::tuple_size_v<Test0Member *> == 1);
+    static_assert(is_mosaic_v<Test0Member *>);
 
-  static_assert(std::is_scalar_v<Test1Member *>);
-  static_assert(!std::is_aggregate_v<Test1Member *>);
-  static_assert(boost::pfr::tuple_size_v<Test1Member *> == 1);
-  static_assert(is_mosaic_v<Test1Member *>);
+    static_assert(std::is_scalar_v<Test1Member *>);
+    static_assert(!std::is_aggregate_v<Test1Member *>);
+    static_assert(boost::pfr::tuple_size_v<Test1Member *> == 1);
+    static_assert(is_mosaic_v<Test1Member *>);
 
-  static_assert(std::is_scalar_v<Test2Members *>);
-  static_assert(!std::is_aggregate_v<Test2Members *>);
-  static_assert(boost::pfr::tuple_size_v<Test2Members *> == 1);
-  static_assert(is_mosaic_v<Test2Members *>);
+    static_assert(std::is_scalar_v<Test2Members *>);
+    static_assert(!std::is_aggregate_v<Test2Members *>);
+    static_assert(boost::pfr::tuple_size_v<Test2Members *> == 1);
+    static_assert(is_mosaic_v<Test2Members *>);
 
-  static_assert(std::is_scalar_v<TestPrivateMembers *>);
-  static_assert(!std::is_aggregate_v<TestPrivateMembers *>);
-  static_assert(boost::pfr::tuple_size_v<TestPrivateMembers *> == 1);
-  static_assert(is_mosaic_v<TestPrivateMembers *>);
+    static_assert(std::is_scalar_v<TestPrivateMembers *>);
+    static_assert(!std::is_aggregate_v<TestPrivateMembers *>);
+    static_assert(boost::pfr::tuple_size_v<TestPrivateMembers *> == 1);
+    static_assert(is_mosaic_v<TestPrivateMembers *>);
 
-  static_assert(std::is_scalar_v<TestRecursive0Member *>);
-  static_assert(!std::is_aggregate_v<TestRecursive0Member *>);
-  static_assert(boost::pfr::tuple_size_v<TestRecursive0Member *> == 1);
-  static_assert(is_mosaic_v<TestRecursive0Member *>);
+    static_assert(std::is_scalar_v<TestRecursive0Member *>);
+    static_assert(!std::is_aggregate_v<TestRecursive0Member *>);
+    static_assert(boost::pfr::tuple_size_v<TestRecursive0Member *> == 1);
+    static_assert(is_mosaic_v<TestRecursive0Member *>);
 
-  static_assert(std::is_scalar_v<TestRecursive1Member *>);
-  static_assert(!std::is_aggregate_v<TestRecursive1Member *>);
-  static_assert(boost::pfr::tuple_size_v<TestRecursive1Member *> == 1);
-  static_assert(is_mosaic_v<TestRecursive1Member *>);
+    static_assert(std::is_scalar_v<TestRecursive1Member *>);
+    static_assert(!std::is_aggregate_v<TestRecursive1Member *>);
+    static_assert(boost::pfr::tuple_size_v<TestRecursive1Member *> == 1);
+    static_assert(is_mosaic_v<TestRecursive1Member *>);
 
-  static_assert(std::is_scalar_v<TestRecursive2Members *>);
-  static_assert(!std::is_aggregate_v<TestRecursive2Members *>);
-  static_assert(boost::pfr::tuple_size_v<TestRecursive2Members *> == 1);
-  static_assert(is_mosaic_v<TestRecursive2Members *>);
+    static_assert(std::is_scalar_v<TestRecursive2Members *>);
+    static_assert(!std::is_aggregate_v<TestRecursive2Members *>);
+    static_assert(boost::pfr::tuple_size_v<TestRecursive2Members *> == 1);
+    static_assert(is_mosaic_v<TestRecursive2Members *>);
+  }
 }
 
 } // namespace ARIA
