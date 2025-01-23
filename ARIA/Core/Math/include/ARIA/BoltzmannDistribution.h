5a2199b1f089003e02b8ca873a290f17acc356ac
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-01-23 17:08:11 +0800

    .

--- ARIA/Core/Math/include/ARIA/BoltzmannDistribution.h
+++ ARIA/Core/Math/include/ARIA/BoltzmannDistribution.h
@@ -1,3 +1,21 @@
 #pragma once
 
 #include "ARIA/Tup.h"
+
+namespace ARIA {
+
+template <uint dim>
+class BoltzmannDistribution;
+
+template <>
+class BoltzmannDistribution<1> {
+public:
+  template <typename TOrder, typename TDomain, typename TU>
+  [[nodiscard]] ARIA_HOST_DEVICE static constexpr Real Moment(const TU &u) {
+    static_assert(tup::detail::is_tec_tr_v<TOrder, uint, 1>, "The given order type should be `Tec1u`");
+    static_assert(tup::detail::is_tec_tr_v<TDomain, int, 1>, "The given domain type should be `Tec1i`");
+    static_assert(tup::detail::is_tec_tr_v<TU, Real, 1>, "The given velocity type should be `Tec1r`");
+  }
+};
+
+} // namespace ARIA
