1f2954923b6621455532cc22819a7dcc53f1f26c
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-01-23 16:57:35 +0800

    Add Boltzmann distribution abstraction.

---
+++ ARIA/Core/Math/include/ARIA/BoltzmannDistribution.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "ARIA/Tup.h"
---
+++ ARIA/Core/Math/tests/BoltzmannDistribution.cpp
@@ -0,0 +1,9 @@
+#include "ARIA/BoltzmannDistribution.h"
+
+#include <gtest/gtest.h>
+
+namespace ARIA {
+
+TEST(BoltzmannDistribution, Base) {}
+
+} // namespace ARIA
