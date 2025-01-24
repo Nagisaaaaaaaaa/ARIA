5214e18943a2c44b3d0080226a58cf1a77226214
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-01-24 16:43:03 +0800

    .

--- ARIA/Core/Math/include/ARIA/MaxwellBoltzmannDistribution.h
+++ ARIA/Core/Math/include/ARIA/MaxwellBoltzmannDistribution.h
@@ -2,6 +2,7 @@
 
 /// \file
 /// \brief A policy-based Maxwell-Boltzmann distribution implementation.
+/// See https://en.wikipedia.org/wiki/Maxwell-Boltzmann_distribution.
 //
 //
 //
@@ -12,6 +13,14 @@
 
 namespace ARIA {
 
+/// \brief A policy-based Maxwell-Boltzmann distribution implementation.
+/// See https://en.wikipedia.org/wiki/Maxwell-Boltzmann_distribution.
+///
+/// The distribution looks like f(ξ, u) = (λ / π)^(d / 2) exp(-λ(ξ - u)^2), where
+/// ξ is the microscopic particle velocity,
+/// u is the macroscopic velocity,
+/// λ = 1 / (2 RT), R is the gas constant, T is the temperature,
+/// d is the dimension of the system.
 template <uint dim, Real lambda>
 class MaxwellBoltzmannDistribution;
 
