aea7f946003193f44b2f1c54a8570b4618d754c5
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-01-26 15:14:21 +0800

    .

--- ARIA/Core/Core/include/ARIA/TypeSet.h
+++ ARIA/Core/Core/include/ARIA/TypeSet.h
@@ -10,78 +10,10 @@
 //
 //
 //
-#include "ARIA/Constant.h"
+#include "ARIA/detail/TypeSetImpl.h"
 
 namespace ARIA {
 
-namespace type_set::detail {
-
-template <typename T>
-struct Wrap {
-  using type = T;
-};
-
-//
-//
-//
-template <size_t i, typename... Ts>
-struct Overloading;
-
-template <size_t i, typename T>
-struct Overloading<i, T> {
-  template <typename U>
-  static consteval C<std::numeric_limits<size_t>::max()> value(U);
-
-  static consteval Wrap<T> type(C<i>);
-  static consteval C<i> value(Wrap<T>);
-};
-
-template <size_t i, typename T, typename... Ts>
-struct Overloading<i, T, Ts...> : Overloading<i + 1, Ts...> {
-  static_assert(decltype(Overloading<i + 1, Ts...>::value(std::declval<Wrap<T>>())){} ==
-                    std::numeric_limits<size_t>::max(),
-                "Duplicated types are not allowed for `TypeSet`");
-
-  using Overloading<i + 1, Ts...>::type;
-  using Overloading<i + 1, Ts...>::value;
-
-  static consteval Wrap<T> type(C<i>);
-  static consteval C<i> value(Wrap<T>);
-};
-
-//
-//
-//
-template <typename... Ts>
-class TypeSetNoCheck {
-private:
-  using TOverloading = Overloading<0, Ts...>;
-
-public:
-  template <size_t i>
-  using Get = typename decltype(TOverloading::type(C<i>{}))::type;
-
-  template <typename T>
-  static constexpr size_t idx = decltype(TOverloading::value(std::declval<Wrap<T>>())){};
-};
-
-//
-//
-//
-template <typename... Ts>
-struct ValidTypeSetImpl {
-  using TNoCheck = TypeSetNoCheck<Ts...>;
-  static constexpr bool value = (std::is_same_v<Ts, TNoCheck::template Get<TNoCheck::template idx<Ts>>> && ...);
-};
-
-template <typename... Ts>
-concept ValidTypeSet = ValidTypeSetImpl<Ts...>::value;
-
-} // namespace type_set::detail
-
-//
-//
-//
 template <typename... Ts>
   requires(type_set::detail::ValidTypeSet<Ts...>)
 class TypeSet {
@@ -95,5 +27,11 @@ public:
   template <typename T>
   static constexpr size_t idx = TNoCheck::template idx<T>;
 };
+
+//
+//
+//
+template <typename... Ts>
+using MakeTypeSet = TypeSet<Ts...>;
 
 } // namespace ARIA
