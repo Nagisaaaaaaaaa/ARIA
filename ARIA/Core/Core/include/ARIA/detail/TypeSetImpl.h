ba56c13b1f6f923989e7961b38bf550e6a2ec5cf
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-01-26 16:10:54 +0800

    .

--- ARIA/Core/Core/include/ARIA/detail/TypeSetImpl.h
+++ ARIA/Core/Core/include/ARIA/detail/TypeSetImpl.h
@@ -6,6 +6,42 @@ namespace ARIA {
 
 namespace type_set::detail {
 
+//! For future developers: Please read the following comments to help you understand the codes.
+//!
+//! `TypeSet` is different from `TypeArray` in that the types are required to be unique.
+//! For example, `TypeSet<int, float>` is OK, but `TypeSet<int, int>` is not allowed.
+//! It seems to be a complex restriction, since we know that duplication check is at least O(n).
+//! But for metaprogramming, this "restriction" allows us to implement the "fastest" `TypeSet`.
+//! That's see how it works.
+//!
+//! Note that all the codes are executed by compilers.
+//! One of the most important features for C++ compilers is to handle function overloading.
+//! That is, given functions such as `void Func(int)`, `void Func(float)`, ...
+//! The "best suited" `Func` should be chosen for any parameters.
+//! So, suppose there are n candidates, we define
+//!   O(Overload(n)): The time the compiler takes to handle function overloading.
+//!
+//! For all metaprogramming codes, this O(Overload(n)) can never be optimized, but
+//! we can assume that O(Overload(n)) is relatively small,
+//! (since the compilers never stuck for very long time, they are really strong, OvO).
+//!
+//! Let's see some examples about how our implementations can influence the compile time:
+//! 1. `std::tuple_element_t<i, T>`:
+//!      Takes O(n(Overload(n) + 1)) to compile because we should loop from `0` to `i`.
+//! 2. `TypeArray::firstIdx<T>`:
+//!      Takes O(n(Overload(n) + 1)) to compile because we should loop from the first type to `T`.
+//!      Although same complexity, this feature is much slower than `std::tuple_element_t` because
+//!      we need to compare the types with `std::is_same_v` at each iteration.
+//!
+//! So, the "fastest" `TypeSet` means that
+//! all operations takes O(Overload(n) + 1) time to compile, which means that
+//!   we have to make everything done at the overloading stage!
+//!
+//! This is impossible for `std::tuple` and `TypeArray`, where types may be duplicated.
+//! But for `TypeSet`, where types are never duplicated, maybe... possible?
+//!
+//! Now, we've got the basic idea.
+
 template <typename T>
 struct Wrap {
   using type = T;
