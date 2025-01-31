a83ca42dfef57580cfc9f94353d7aaa3bf5af676
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-01-31 20:35:11 +0800

    .

--- ARIA/Core/Core/include/ARIA/Buyout.h
+++ ARIA/Core/Core/include/ARIA/Buyout.h
@@ -5,8 +5,12 @@
 /// Here, "multiple" means that it may be called 0, 1, 2, ... times, and
 /// the number is likely to be but not always > 0.
 ///
-/// It's OK to just call it, but for performance-sensitive codes, it is
-/// usually faster to call it at least once and store the value for later usages.
+/// For example, there is a task where:
+/// 1. For 95% cases, we need to do 10 things for 5 times (totally 10 * 5 things).
+/// 2. For  5% cases, we need to do  9 things for 5 times (totally  9 * 5 things).
+///
+/// It's OK to just do it or use `[[likely]]`, but for performance-sensitive codes, it is
+/// usually faster to always do 10 things and store the 10 values for later usages.
 /// Especially for GPU codes, where `if-else` may slow down too much.
 ///
 /// That's the motivation of introducing `Buyout`.
@@ -14,11 +18,6 @@
 /// store the values for later usages.
 /// Here, the arguments are required to be determined at compile-time, which
 /// means that no runtime `if-else` is allowed at the construction stage.
-///
-/// For example, `Buyout` may be helpful for tasks where:
-/// 1. for 95% cases, we need to do 10 things for 5 times (totally 10 * 5).
-/// 2. for the remaining 5% cases, we only need to do 9 things for 5 times (totally 9 * 5).
-///
 
 //
 //
