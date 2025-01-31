e20f97351801dbc6f10ba92e0df03354eb5d8a12
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-01-31 16:05:35 +0800

    .

--- ARIA/Core/Core/tests/Buyout.cpp
+++ ARIA/Core/Core/tests/Buyout.cpp
@@ -13,20 +13,60 @@ struct SizeOf {
   }
 };
 
+struct Construct {
+  template <typename T>
+  constexpr T operator()() const {
+    if constexpr (std::integral<T>)
+      return T(9);
+    else
+      return T{9};
+  }
+};
+
 } // namespace
 
 TEST(Buyout, Base) {
-  constexpr Buyout<SizeOf, int8_t, int16_t, int32_t, int64_t> buyout{SizeOf{}};
+  {
+    constexpr Buyout<SizeOf, int8_t, int16_t, int32_t, int64_t> buyout{SizeOf{}};
 
-  static_assert(buyout.operator()<int8_t>() == 1);
-  static_assert(buyout.operator()<int16_t>() == 2);
-  static_assert(buyout.operator()<int32_t>() == 4);
-  static_assert(buyout.operator()<int64_t>() == 8);
+    static_assert(buyout.operator()<int8_t>() == 1);
+    static_assert(buyout.operator()<int16_t>() == 2);
+    static_assert(buyout.operator()<int32_t>() == 4);
+    static_assert(buyout.operator()<int64_t>() == 8);
 
-  static_assert(get<int8_t>(buyout) == 1);
-  static_assert(get<int16_t>(buyout) == 2);
-  static_assert(get<int32_t>(buyout) == 4);
-  static_assert(get<int64_t>(buyout) == 8);
+    static_assert(get<int8_t>(buyout) == 1);
+    static_assert(get<int16_t>(buyout) == 2);
+    static_assert(get<int32_t>(buyout) == 4);
+    static_assert(get<int64_t>(buyout) == 8);
+  }
+
+  {
+    constexpr Buyout<Construct, int8_t, int16_t, int32_t, int64_t, std::array<int, 1>> buyout{Construct{}};
+
+    static_assert(std::is_same_v<decltype(buyout.operator()<int8_t>()), const int8_t &>);
+    static_assert(std::is_same_v<decltype(buyout.operator()<int16_t>()), const int16_t &>);
+    static_assert(std::is_same_v<decltype(buyout.operator()<int32_t>()), const int32_t &>);
+    static_assert(std::is_same_v<decltype(buyout.operator()<int64_t>()), const int64_t &>);
+    static_assert(std::is_same_v<decltype(buyout.operator()<std::array<int, 1>>()), const std::array<int, 1> &>);
+
+    static_assert(std::is_same_v<decltype(get<int8_t>(buyout)), const int8_t &>);
+    static_assert(std::is_same_v<decltype(get<int16_t>(buyout)), const int16_t &>);
+    static_assert(std::is_same_v<decltype(get<int32_t>(buyout)), const int32_t &>);
+    static_assert(std::is_same_v<decltype(get<int64_t>(buyout)), const int64_t &>);
+    static_assert(std::is_same_v<decltype(get<std::array<int, 1>>(buyout)), const std::array<int, 1> &>);
+
+    static_assert(buyout.operator()<int8_t>() == 9);
+    static_assert(buyout.operator()<int16_t>() == 9);
+    static_assert(buyout.operator()<int32_t>() == 9);
+    static_assert(buyout.operator()<int64_t>() == 9);
+    static_assert(buyout.operator()<std::array<int, 1>>() == std::array{9});
+
+    static_assert(get<int8_t>(buyout) == 9);
+    static_assert(get<int16_t>(buyout) == 9);
+    static_assert(get<int32_t>(buyout) == 9);
+    static_assert(get<int64_t>(buyout) == 9);
+    static_assert(get<std::array<int, 1>>(buyout) == std::array{9});
+  }
 }
 
 } // namespace ARIA
