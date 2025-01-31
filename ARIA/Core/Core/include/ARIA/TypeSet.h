f1d495e1a436b79ba170e110832b692c11c8bb23
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-01-31 14:20:17 +0800

    .

--- ARIA/Core/Core/include/ARIA/TypeSet.h
+++ ARIA/Core/Core/include/ARIA/TypeSet.h
@@ -59,6 +59,39 @@ public:
 
   template <typename Void = void>
   using Reverse = MakeTypeSet<typename TArray::template Reverse<>>;
+
+  template <Idx i>
+  using Erase = MakeTypeSet<typename TArray::template Erase<i>>;
+
+  template <Idx i, typename T>
+  using Insert = MakeTypeSet<typename TArray::template Insert<i, T>>;
+
+  template <typename Void = void>
+  using PopFront = MakeTypeSet<typename TArray::template PopFront<>>;
+
+  template <typename Void = void>
+  using PopBack = MakeTypeSet<typename TArray::template PopBack<>>;
+
+  template <typename T>
+  using PushFront = MakeTypeSet<typename TArray::template PushFront<T>>;
+
+  template <typename T>
+  using PushBack = MakeTypeSet<typename TArray::template PushBack<T>>;
+
+  template <type_array::detail::NonArrayType T>
+  using Remove = MakeTypeSet<typename TArray::template Remove<T>>;
+
+  template <type_array::detail::NonArrayType TFrom, typename TTo>
+  using Replace = MakeTypeSet<typename TArray::template Replace<TFrom, TTo>>;
+
+  //
+  //
+  //
+  template <template <type_array::detail::NonArrayType> typename F>
+  using ForEach = MakeTypeSet<typename TArray::template ForEach<F>>;
+
+  template <template <type_array::detail::NonArrayType> typename F>
+  using Filter = MakeTypeSet<typename TArray::template Filter<F>>;
 };
 
 } // namespace ARIA
