6f0ade0af2fe38f1627723698608c93cf41eb7cf
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-01 15:38:18 +0800

    .

--- ARIA/Core/Core/include/ARIA/detail/MosaicIterator.h
+++ ARIA/Core/Core/include/ARIA/detail/MosaicIterator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ARIA/Mosaic.h"
+#include "ARIA/Tup.h"
 
 #include <boost/iterator/transform_iterator.hpp>
 #include <boost/iterator/zip_iterator.hpp>
@@ -9,13 +10,14 @@ namespace ARIA {
 
 template <typename TMosaic, typename... TIterators>
   requires(mosaic::detail::is_mosaic_v<TMosaic>)
-ARIA_HOST_DEVICE static constexpr auto make_mosaic_iterator(TIterators &&...iterators) {
+ARIA_HOST_DEVICE static constexpr auto make_mosaic_iterator(Tup<TIterators...> iterators) {
   static_assert(mosaic::detail::ValidMosaic<TMosaic>, "The mosaic definition is invalid");
   using TMosaicPattern = typename mosaic::detail::is_mosaic<TMosaic>::TMosaicPattern;
 
-  return boost::make_transform_iterator(
-      boost::make_zip_iterator(boost::make_tuple(std::forward<TIterators>(iterators)...)),
-      [](const auto &z) -> TMosaicPattern {
+  boost::tuple<TIterators...> iteratorsBoost;
+  ForEach<sizeof...(TIterators)>([&]<auto i>() { get<i>(iteratorsBoost) = get<i>(iterators); });
+
+  return boost::make_transform_iterator(boost::make_zip_iterator(iteratorsBoost), [](const auto &z) -> TMosaicPattern {
     TMosaicPattern res;
     ForEach<sizeof...(TIterators)>([&]<auto i>() {
       static_assert(std::is_same_v<std::decay_t<decltype(mosaic::detail::get_recursive<i>(res))>,
@@ -25,6 +27,12 @@ ARIA_HOST_DEVICE static constexpr auto make_mosaic_iterator(TIterators &&...iter
     });
     return res;
   });
+}
+
+template <typename TNonMosaic, typename TIterator>
+  requires(!mosaic::detail::is_mosaic_v<TNonMosaic>)
+ARIA_HOST_DEVICE static constexpr auto make_mosaic_iterator(TIterator iterator) {
+  return iterator;
 }
 
 } // namespace ARIA
