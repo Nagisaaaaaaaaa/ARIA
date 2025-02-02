ea4c3831adbef590a48a8f9a0c841afa0e569fb8
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-02 15:26:26 +0800

    .

--- ARIA/Core/Core/tests/detail/MosaicVector.cu
+++ ARIA/Core/Core/tests/detail/MosaicVector.cu
@@ -4,8 +4,50 @@
 
 namespace ARIA {
 
+namespace {
+
+struct PatternIF {
+  int i;
+  float f;
+};
+
+struct PatternIII {
+  struct {
+    int v0;
+    int v1;
+  } ii;
+
+  int v2;
+};
+
+} // namespace
+
+template <>
+struct Mosaic<Tup<int, float>, PatternIF> {
+  PatternIF operator()(const Tup<int, float> &v) const { return {.i = get<0>(v), .f = get<1>(v)}; }
+
+  Tup<int, float> operator()(const PatternIF &v) const { return {v.i, v.f}; }
+};
+
+template <>
+struct Mosaic<Tup<int, int, int>, PatternIII> {
+  PatternIII operator()(const Tup<int, int, int> &v) const {
+    return {.ii = {.v0 = get<0>(v), .v1 = get<1>(v)}, .v2 = get<2>(v)};
+  }
+
+  Tup<int, int, int> operator()(const PatternIII &v) const { return {v.ii.v0, v.ii.v1, v.v2}; }
+};
+
 TEST(MosaicVector, Base) {
   using namespace mosaic::detail;
+
+  // `int, float`.
+  {
+    using T = Tup<int, float>;
+    using TMosaic = Mosaic<T, PatternIF>;
+
+    MosaicVector<TMosaic> v;
+  }
 }
 
 } // namespace ARIA
