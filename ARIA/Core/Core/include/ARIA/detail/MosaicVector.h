a16c85bf742185a8a922ad3e61c63bdeaf426bb5
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-02 15:22:11 +0800

    .

--- ARIA/Core/Core/include/ARIA/detail/MosaicVector.h
+++ ARIA/Core/Core/include/ARIA/detail/MosaicVector.h
@@ -2,10 +2,49 @@
 
 #include "ARIA/detail/MosaicIterator.h"
 
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+
 namespace ARIA {
 
 namespace mosaic::detail {
 
+template <MosaicPattern TMosaicPattern, typename TSpaceHostOrDevice>
+struct reduce_mosaic_vector_storage_type;
+
+template <MosaicPattern TMosaicPattern>
+struct reduce_mosaic_vector_storage_type<TMosaicPattern, SpaceHost> {
+private:
+  template <type_array::detail::NonArrayType... Ts>
+  consteval auto impl(MakeTypeArray<Ts...>) {
+    using TStorage = Tup<thrust::host_vector<Ts...>>;
+    return TStorage{};
+  }
+
+public:
+  using type = decltype(impl(std::declval<mosaic_pattern_types_recursive_t<TMosaicPattern>>()));
+};
+
+template <MosaicPattern TMosaicPattern>
+struct reduce_mosaic_vector_storage_type<TMosaicPattern, SpaceDevice> {
+private:
+  template <type_array::detail::NonArrayType... Ts>
+  consteval auto impl(MakeTypeArray<Ts...>) {
+    using TStorage = Tup<thrust::device_vector<Ts...>>;
+    return TStorage{};
+  }
+
+public:
+  using type = decltype(impl(std::declval<mosaic_pattern_types_recursive_t<TMosaicPattern>>()));
+};
+
+template <MosaicPattern TMosaicPattern, typename TSpaceHostOrDevice>
+using reduce_mosaic_vector_storage_type_t =
+    typename reduce_mosaic_vector_storage_type<TMosaicPattern, TSpaceHostOrDevice>::type;
+
+//
+//
+//
 template <typename TMosaic, typename... Ts>
   requires(is_mosaic_v<TMosaic>)
 class MosaicVector final {
@@ -14,9 +53,15 @@ private:
 
   using T = typename is_mosaic<TMosaic>::T;
   using TMosaicPattern = typename is_mosaic<TMosaic>::TMosaicPattern;
+  using TStorage = reduce_mosaic_vector_storage_type_t<TMosaicPattern, SpaceHost>;
+
+  static constexpr size_t size = tuple_size_recursive_v<TMosaicPattern>;
 
 public:
   using value_type = T;
+
+private:
+  TStorage storage_;
 };
 
 } // namespace mosaic::detail
