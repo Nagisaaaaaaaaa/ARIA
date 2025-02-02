a811efca66e9d9a32bb59b85195c453f57d5f740
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-02 15:02:57 +0800

    .

--- ARIA/Core/Core/include/ARIA/detail/MosaicVector.h
+++ ARIA/Core/Core/include/ARIA/detail/MosaicVector.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "ARIA/detail/MosaicIterator.h"
+
+namespace ARIA {
+
+namespace mosaic::detail {
+
+template <typename TMosaic, typename... Ts>
+  requires(is_mosaic_v<TMosaic>)
+class MosaicVector final {
+private:
+  static_assert(ValidMosaic<TMosaic>, "The mosaic definition is invalid");
+
+  using T = typename is_mosaic<TMosaic>::T;
+  using TMosaicPattern = typename is_mosaic<TMosaic>::TMosaicPattern;
+
+public:
+  using value_type = T;
+};
+
+} // namespace mosaic::detail
+
+} // namespace ARIA
---
+++ ARIA/Core/Core/tests/detail/MosaicVector.cu
@@ -0,0 +1,11 @@
+#include "ARIA/detail/MosaicVector.h"
+
+#include <gtest/gtest.h>
+
+namespace ARIA {
+
+TEST(MosaicVector, Base) {
+  using namespace mosaic::detail;
+}
+
+} // namespace ARIA
