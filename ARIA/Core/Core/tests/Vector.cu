9f76559ccb0823384cc02d4fc94302924c1c207b
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-04 01:29:50 +0800

    .

--- ARIA/Core/Core/tests/Vector.cu
+++ ARIA/Core/Core/tests/Vector.cu
@@ -1,3 +1,4 @@
+#include "ARIA/Let.h"
 #include "ARIA/Vector.h"
 
 #include <gtest/gtest.h>
@@ -65,6 +66,97 @@ TEST(Vector, Base) {
 
       static_assert(std::is_same_v<Vector<T, SpaceDevice>, thrust::device_vector<T>>);
       static_assert(std::is_same_v<VectorDevice<T>, thrust::device_vector<T>>);
+    });
+  }
+
+  // `int, float`.
+  {
+    using T = Tup<int, float>;
+    using TMosaic = Mosaic<T, PatternIF>;
+
+    ForEach<MakeTypeArray<     //
+        VectorHost<TMosaic>,   //
+        VectorDevice<TMosaic>, //
+        VectorHost<T>,         //
+        VectorDevice<T>        //
+        >>([]<typename TVector>() {
+      TVector vec;
+      EXPECT_EQ(vec.size(), 0);
+
+      vec.resize(5);
+      EXPECT_EQ(vec.size(), 5);
+
+      for (int i = 0; i < 5; ++i) {
+        T v = vec[i];
+        EXPECT_EQ(get<0>(v), 0);
+        EXPECT_FLOAT_EQ(get<1>(v), 0.0F);
+
+        vec[i] = T{i, i + (i + 1) * 0.1F};
+        v = vec[i];
+        EXPECT_EQ(get<0>(v), i);
+        EXPECT_FLOAT_EQ(get<1>(v), i + (i + 1) * 0.1F);
+      }
+
+      for (let it = vec.begin(); it != vec.end(); ++it) {
+        T v = *it;
+        let k = it - vec.begin();
+        int i = k;
+        static_assert(std::is_same_v<decltype(k), int64>);
+        EXPECT_EQ(get<0>(v), i);
+        EXPECT_FLOAT_EQ(get<1>(v), i + (i + 1) * 0.1F);
+
+        *it = T{0, 0.0F};
+        v = vec[i];
+        EXPECT_EQ(get<0>(v), 0);
+        EXPECT_FLOAT_EQ(get<1>(v), 0.0F);
+      }
+
+      for (let it = vec.cbegin(); it != vec.cend(); ++it) {
+        T v = *it;
+        let k = it - vec.cbegin();
+        int i = k;
+        static_assert(std::is_same_v<decltype(k), int64>);
+        EXPECT_EQ(get<0>(v), 0);
+        EXPECT_FLOAT_EQ(get<1>(v), 0.0F);
+      }
+
+      for (int i = 0; i < 5; ++i) {
+        let ptr = vec.data() + i;
+        T v = *ptr;
+        EXPECT_EQ(get<0>(v), 0);
+        EXPECT_FLOAT_EQ(get<1>(v), 0.0F);
+
+        *ptr = T{i, i + (i + 1) * 0.1F};
+        v = *ptr;
+        EXPECT_EQ(get<0>(v), i);
+        EXPECT_FLOAT_EQ(get<1>(v), i + (i + 1) * 0.1F);
+      }
+
+      {
+        TVector vec1(5);
+        EXPECT_EQ(vec1.size(), 5);
+        for (int i = 0; i < 5; ++i) {
+          T v = vec1[i];
+          EXPECT_EQ(get<0>(v), 0);
+          EXPECT_FLOAT_EQ(get<1>(v), 0.0F);
+        }
+
+        vec1.clear();
+        EXPECT_EQ(vec1.size(), 0);
+      }
+
+      {
+        TVector vec1{T{0, 0.1F}, T{1, 1.2F}, T{2, 2.3F}, T{3, 3.4F}, T{4, 4.5F}};
+        EXPECT_EQ(vec1.size(), 5);
+        for (int i = 0; i < 5; ++i) {
+          T v = vec1[i];
+          EXPECT_EQ(get<0>(v), i);
+          EXPECT_FLOAT_EQ(get<1>(v), i + (i + 1) * 0.1F);
+        }
+
+        vec1.clear();
+        EXPECT_EQ(vec1.size(), 0);
+      }
     });
   }
 }
