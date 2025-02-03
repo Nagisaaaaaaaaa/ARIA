8e06fabb03d208f0b59a2de3a3a117089e7739eb
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-03 20:59:44 +0800

    .

---
+++ ARIA/Core/Core/include/ARIA/Vector.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "ARIA/detail/MosaicVector.h"
---
+++ ARIA/Core/Core/tests/Vector.cu
@@ -0,0 +1,9 @@
+#include "ARIA/Vector.h"
+
+#include <gtest/gtest.h>
+
+namespace ARIA {
+
+TEST(Vector, Base) {}
+
+} // namespace ARIA
