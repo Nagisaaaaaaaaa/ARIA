6e723854b5488bfc49676263260020c2efc05e0b
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-04 01:49:18 +0800

    .

--- ARIA/Core/Core/include/ARIA/Vector.h
+++ ARIA/Core/Core/include/ARIA/Vector.h
@@ -4,6 +4,31 @@
 
 namespace ARIA {
 
+/// struct Pattern {
+///   struct {
+///     int v0;
+///     int v1;
+///   } v01;
+///
+///   int v2;
+/// };
+///
+/// template <>
+/// struct Mosaic<Tec<int, int, int>, Pattern> {
+///   Pattern operator()(const Tec<int, int, int> &v) const {
+///     return {.v01 = {.v0 = get<0>(v), .v1 = get<1>(v)}, .v2 = get<2>(v)};
+///   }
+///
+///   Tec<int, int, int> operator()(const Pattern &v) const { return {v.v01.v0, v.v01.v1, v.v2}; }
+/// };
+///
+/// using T = Tec<int, int, int>;
+/// using TMosaic = Mosaic<T, Pattern>;
+///
+/// VectorHost<T> v0;         // Host   vector with array of structures (AoS).
+/// VectorDevice<T> v1;       // Device vector with array of structures (AoS).
+/// VectorHost<TMosaic> v2;   // Host   vector with structure of arrays (SoA).
+/// VectorDevice<TMosaic> v3; // Device vector with structure of arrays (SoA).
 template <typename T, typename TSpaceHostOrDevice, typename... Ts>
 using Vector = mosaic::detail::reduce_vector_t<T, TSpaceHostOrDevice, Ts...>;
 
