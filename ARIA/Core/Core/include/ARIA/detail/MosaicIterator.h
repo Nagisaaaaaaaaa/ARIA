82ee6572eab08723b6eba28c9fc0eb94a0d76b1e
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-03 17:39:25 +0800

    .

--- ARIA/Core/Core/include/ARIA/detail/MosaicIterator.h
+++ ARIA/Core/Core/include/ARIA/detail/MosaicIterator.h
@@ -211,7 +211,7 @@ static constexpr auto MosaicPointer2Tup(const TMosaicPointer &pointer) {
 //
 //
 //
-// Implementation of `copy` for "mosaic iterators".
+// Implementation of `copy` for `MosaicIterator`s.
 template <MosaicIterator TItIn, MosaicIterator TItOut>
 TItOut copy(TItIn srcBegin, TItIn srcEnd, TItOut dst) {
   using TMosaic = typename decltype(*dst)::TMosaic;
