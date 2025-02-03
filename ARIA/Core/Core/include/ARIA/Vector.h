cb5ce2ec456c1ff557f7b334625c356a32fe4b28
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-04 01:14:17 +0800

    .

--- ARIA/Core/Core/include/ARIA/Vector.h
+++ ARIA/Core/Core/include/ARIA/Vector.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "ARIA/detail/MosaicVector.h"
+
+namespace ARIA {
+
+template <typename T, typename TSpaceHostOrDevice, typename... Ts>
+using Vector = mosaic::detail::reduce_vector_t<T, TSpaceHostOrDevice, Ts...>;
+
+template <typename T, typename... Ts>
+using VectorHost = Vector<T, SpaceHost, Ts...>;
+
+template <typename T, typename... Ts>
+using VectorDevice = Vector<T, SpaceDevice, Ts...>;
+
+} // namespace ARIA
