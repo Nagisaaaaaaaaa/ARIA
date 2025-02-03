561f6ddf03a3b7716771bbedcf7305b56cfadf45
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-04 01:30:38 +0800

    .

--- ARIA/Core/Core/tests/Vector.cu
+++ ARIA/Core/Core/tests/Vector.cu
@@ -114,7 +114,6 @@ TEST(Vector, Base) {
       for (let it = vec.cbegin(); it != vec.cend(); ++it) {
         T v = *it;
         let k = it - vec.cbegin();
-        int i = k;
         static_assert(std::is_same_v<decltype(k), int64>);
         EXPECT_EQ(get<0>(v), 0);
         EXPECT_FLOAT_EQ(get<1>(v), 0.0F);
