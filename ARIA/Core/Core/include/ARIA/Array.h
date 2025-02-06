501bd58457bb5d26f41fbec06f07f7aae5e45274
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-06 20:59:34 +0800

    .

--- ARIA/Core/Core/include/ARIA/Array.h
+++ ARIA/Core/Core/include/ARIA/Array.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "ARIA/detail/MosaicArray.h"
+
+namespace ARIA {
+
+template <typename T, size_t size>
+using Array = mosaic::detail::reduce_array_t<T, size>;
+
+} // namespace ARIA
