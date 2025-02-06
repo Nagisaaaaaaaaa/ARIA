fdd903a8143de8b28f4691d023152d461abe739d
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-06 18:54:05 +0800

    .

---
+++ ARIA/Core/Core/include/ARIA/detail/MosaicArray.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "ARIA/detail/MosaicIterator.h"
+
+namespace ARIA {
+
+namespace mosaic::detail {
+
+} // namespace mosaic::detail
+
+} // namespace ARIA
---
+++ ARIA/Core/Core/tests/detail/MosaicArray.cu
@@ -0,0 +1,46 @@
+#include "ARIA/Let.h"
+#include "ARIA/detail/MosaicArray.h"
+
+#include <gtest/gtest.h>
+
+namespace ARIA {
+
+namespace {
+
+struct PatternIF {
+  int i;
+  float f;
+};
+
+struct PatternIII {
+  struct {
+    int v0;
+    int v1;
+  } ii;
+
+  int v2;
+};
+
+} // namespace
+
+template <>
+struct Mosaic<Tup<int, float>, PatternIF> {
+  PatternIF operator()(const Tup<int, float> &v) const { return {.i = get<0>(v), .f = get<1>(v)}; }
+
+  Tup<int, float> operator()(const PatternIF &v) const { return {v.i, v.f}; }
+};
+
+template <>
+struct Mosaic<Tec<int, int, int>, PatternIII> {
+  PatternIII operator()(const Tec<int, int, int> &v) const {
+    return {.ii = {.v0 = get<0>(v), .v1 = get<1>(v)}, .v2 = get<2>(v)};
+  }
+
+  Tec<int, int, int> operator()(const PatternIII &v) const { return {v.ii.v0, v.ii.v1, v.v2}; }
+};
+
+TEST(MosaicArray, Base) {
+  using namespace mosaic::detail;
+}
+
+} // namespace ARIA
