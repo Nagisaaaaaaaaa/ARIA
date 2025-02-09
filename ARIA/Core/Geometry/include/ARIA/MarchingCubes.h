b63eb12dab76451b7df77652a016a0e6465264e0
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-09 19:30:53 +0800

    .

---
+++ ARIA/Core/Geometry/include/ARIA/MarchingCubes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ARIA/Vec.h"
+
+namespace ARIA {
+
+template <uint dim>
+class MarchingCubes;
+
+template <>
+class MarchingCubes<2> {
+public:
+};
+
+} // namespace ARIA
---
+++ ARIA/Core/Geometry/tests/MarchingCubes.cpp
@@ -0,0 +1,9 @@
+#include "ARIA/MarchingCubes.h"
+
+#include <gtest/gtest.h>
+
+namespace ARIA {
+
+TEST(MarchingCubes, Base) {}
+
+} // namespace ARIA
