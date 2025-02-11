dc8f1d2b820cf5a18d7e60d3fa2aa8f0298ac295
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-11 17:14:23 +0800

    .

--- ARIA/Core/Geometry/tests/MarchingCube.cu
+++ ARIA/Core/Geometry/tests/MarchingCube.cu
@@ -10,14 +10,33 @@ TEST(MarchingCube, Base) {
   // 1D.
   {
     using MC = MarchingCube<1>;
-    Vec1r positions[2] = {Vec1r{-2.5_R}, Vec1r{2.5_R}};
-    Real values[2] = {0.1_R, 0.8_R};
+
+    auto testExtract0 = [&](const auto &positions, const auto &values, Real isoValue) {
+      MC::Extract([&](uint i) { return positions[i]; }, [&](uint i) { return values[i]; }, isoValue,
+                  [&](cuda::std::array<Vec1r, 1>) { EXPECT_FALSE(true); });
+    };
+
+    auto testExtract1 = [&](const auto &positions, const auto &values, Real isoValue) {
+      uint times = 0;
+      MC::Extract([&](uint i) { return positions[i]; }, [&](uint i) { return values[i]; }, isoValue,
+                  [&](cuda::std::array<Vec1r, 1> primitiveVertices) {
+        EXPECT_EQ(times, 0);
+        Vec1r p = Lerp(positions[0], positions[1], computeT(values[0], values[1], isoValue));
+        EXPECT_FLOAT_EQ(primitiveVertices[0].x(), p.x());
+      });
+    };
+
+    std::array positions = {Vec1r{-2.5_R}, Vec1r{2.5_R}};
+    std::array values0 = {0.1_R, 0.1_R};
+    std::array values1 = {0.8_R, 0.1_R};
+    std::array values2 = {0.1_R, 0.8_R};
+    std::array values3 = {0.8_R, 0.8_R};
     Real isoValue = 0.4_R;
-    MC::Extract([&](uint i) { return positions[i]; }, [&](uint i) { return values[i]; }, isoValue,
-                [&](cuda::std::array<Vec1r, 1> primitiveVertices) {
-      Vec1r p = Lerp(positions[0], positions[1], computeT(values[0], values[1], isoValue));
-      EXPECT_FLOAT_EQ(primitiveVertices[0].x(), p.x());
-    });
+
+    testExtract0(positions, values0, isoValue);
+    testExtract1(positions, values1, isoValue);
+    testExtract1(positions, values2, isoValue);
+    testExtract0(positions, values3, isoValue);
   }
 }
 
