e15b067ab1f3a9a48b52d5d2630b74548b7b7512
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-11 17:03:54 +0800

    .

--- ARIA/Core/Geometry/tests/MarchingCube.cu
+++ ARIA/Core/Geometry/tests/MarchingCube.cu
@@ -5,13 +5,19 @@
 namespace ARIA {
 
 TEST(MarchingCube, Base) {
+  auto computeT = [](Real v0, Real v1, Real isoValue) { return (isoValue - v0) / (v1 - v0); };
+
   // 1D.
   {
     using MC = MarchingCube<1>;
-    Vec1r positions[2] = {Vec1r{0.0_R}, Vec1r{2.0_R}};
-    Real values[2] = {0.0_R, 0.0_R};
-    MC::Extract([&](uint i) { return positions[i]; }, [&](uint i) { return values[i]; }, 0.5_R,
-                [](cuda::std::array<Vec1r, 1> primitiveVertices) { fmt::println("{}", primitiveVertices[0].x()); });
+    Vec1r positions[2] = {Vec1r{-2.5_R}, Vec1r{2.5_R}};
+    Real values[2] = {0.1_R, 0.8_R};
+    Real isoValue = 0.4_R;
+    MC::Extract([&](uint i) { return positions[i]; }, [&](uint i) { return values[i]; }, isoValue,
+                [&](cuda::std::array<Vec1r, 1> primitiveVertices) {
+      Vec1r p = Lerp(positions[0], positions[1], computeT(values[0], values[1], isoValue));
+      EXPECT_FLOAT_EQ(primitiveVertices[0].x(), p.x());
+    });
   }
 }
 
