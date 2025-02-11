150e57ad4fac5754bd131cab2001c10aac03d379
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-11 17:47:16 +0800

    .

--- ARIA/Core/Geometry/tests/MarchingCube.cu
+++ ARIA/Core/Geometry/tests/MarchingCube.cu
@@ -40,6 +40,34 @@ TEST(MarchingCube, Base) {
     testExtract1(positions, values2, isoValue);
     testExtract0(positions, values3, isoValue);
   }
+
+  // 2D.
+  {
+    using MC = MarchingCube<2>;
+
+    auto testExtract0 = [&](const auto &positions, const auto &values, Real isoValue) {
+      MC::Extract(positions, values, isoValue, [&](const cuda::std::span<Vec2r, 2> &) { EXPECT_FALSE(true); });
+    };
+
+    auto testExtract1_OO = [&](const auto &positions, const auto &values, Real isoValue) {
+      uint times = 0;
+      MC::Extract(positions, values, isoValue, [&](const cuda::std::span<Vec2r, 2> &primitiveVertices) {
+        EXPECT_EQ(times, 0);
+        ++times;
+        Vec2r p_OO_PO = Lerp(positions(0, 0), positions(1, 0), computeT(values(0, 0), values(1, 0), isoValue));
+        Vec2r p_OO_OP = Lerp(positions(0, 0), positions(0, 1), computeT(values(0, 0), values(0, 1), isoValue));
+        fmt::print("{} {} {} {}", p_OO_PO.x(), p_OO_PO.y(), p_OO_OP.x(), p_OO_OP.y());
+        fmt::print("{} {} {} {}", primitiveVertices[0].x(), primitiveVertices[0].y(), primitiveVertices[1].x(),
+                   primitiveVertices[1].y());
+      });
+      EXPECT_EQ(times, 1);
+    };
+
+    auto positions = [](uint i, uint j) {
+      return std::array{std::array{Vec2r{-2.5_R, -2.5_R}, Vec2r{-2.5_R, 2.5_R}}, //
+                        std::array{Vec2r{2.5_R, -2.5_R}, Vec2r{2.5_R, 2.5_R}}}[i][j];
+    };
+  }
 }
 
 } // namespace ARIA
