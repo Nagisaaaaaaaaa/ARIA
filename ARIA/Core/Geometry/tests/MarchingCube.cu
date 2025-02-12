0dba5b76718146b80cce73b9a58d258f3aa45a66
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-12 14:21:49 +0800

    .

--- ARIA/Core/Geometry/tests/MarchingCube.cu
+++ ARIA/Core/Geometry/tests/MarchingCube.cu
@@ -126,6 +126,22 @@ TEST(MarchingCube, Base) {
       EXPECT_EQ(times, 1);
     };
 
+    auto testExtract_BBAA = [&](const auto &positions, const auto &values, Real isoValue) {
+      uint times = 0;
+      MC::Extract(positions, values, isoValue, [&](const cuda::std::span<Vec2r, 2> &primitiveVertices) {
+        EXPECT_EQ(times, 0);
+        ++times;
+        sortPrimitiveVertices(primitiveVertices);
+        Vec2r p_00_10 = Lerp(positions(0, 0), positions(1, 0), computeT(values(0, 0), values(1, 0), isoValue));
+        Vec2r p_01_11 = Lerp(positions(0, 1), positions(1, 1), computeT(values(0, 1), values(1, 1), isoValue));
+        EXPECT_FLOAT_EQ(primitiveVertices[0].x(), p_00_10.x());
+        EXPECT_FLOAT_EQ(primitiveVertices[0].y(), p_00_10.y());
+        EXPECT_FLOAT_EQ(primitiveVertices[1].x(), p_01_11.x());
+        EXPECT_FLOAT_EQ(primitiveVertices[1].y(), p_01_11.y());
+      });
+      EXPECT_EQ(times, 1);
+    };
+
     auto positions = [](uint i, uint j) {
       return arr{arr{Vec2r{-2.5_R, -2.5_R}, Vec2r{-2.5_R, 2.5_R}}, //
                  arr{Vec2r{2.5_R, -2.5_R}, Vec2r{2.5_R, 2.5_R}}}[i][j];
@@ -146,6 +162,9 @@ TEST(MarchingCube, Base) {
     auto values_OOOP = [](uint i, uint j) { return arr{arr{0.1_R, 0.1_R}, arr{0.1_R, 0.8_R}}[i][j]; };
     auto values_PPPO = [](uint i, uint j) { return arr{arr{0.8_R, 0.8_R}, arr{0.8_R, 0.1_R}}[i][j]; };
 
+    auto values_PPOO = [](uint i, uint j) { return arr{arr{0.8_R, 0.8_R}, arr{0.1_R, 0.1_R}}[i][j]; };
+    auto values_OOPP = [](uint i, uint j) { return arr{arr{0.1_R, 0.1_R}, arr{0.8_R, 0.8_R}}[i][j]; };
+
     Real isoValue = 0.4_R;
 
     testExtract_AAAA(positions, values_OOOO, isoValue);
@@ -162,6 +181,9 @@ TEST(MarchingCube, Base) {
 
     testExtract_AAAB(positions, values_OOOP, isoValue);
     testExtract_AAAB(positions, values_PPPO, isoValue);
+
+    testExtract_BBAA(positions, values_PPOO, isoValue);
+    testExtract_BBAA(positions, values_OOPP, isoValue);
   }
 }
 
