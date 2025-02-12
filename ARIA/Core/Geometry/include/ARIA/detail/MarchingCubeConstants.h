b46524b92fc275a8473ec69f356a794d9a143c77
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-12 14:47:31 +0800

    .

--- ARIA/Core/Geometry/include/ARIA/detail/MarchingCubeConstants.h
+++ ARIA/Core/Geometry/include/ARIA/detail/MarchingCubeConstants.h
@@ -19,7 +19,7 @@ ARIA_CONST static inline constexpr int8 vtkMarchingCubes2DLineCases[16][5] = {
     {3, 2, -1, -1, -1},   {0, 2, -1, -1, -1}, {1, 0, 3, 2, -1},   {1, 2, -1, -1, -1},
     {3, 1, -1, -1, -1},   {0, 1, -1, -1, -1}, {3, 0, -1, -1, -1}, {-1, -1, -1, -1, -1}};
 
-ARIA_CONST static inline constexpr int8 vtkMarchingCubes2DEdges[4][2] = {{0, 1}, {1, 3}, {2, 3}, {0, 2}};
+ARIA_CONST static inline constexpr int8 vtkMarchingCubes2DEdges[4][2] = {{0, 1}, {1, 2}, {3, 2}, {0, 3}};
 
 //
 //
