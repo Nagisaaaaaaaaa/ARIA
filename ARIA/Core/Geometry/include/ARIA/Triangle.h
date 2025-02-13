2af4a0846bee3e99a2c54e0e6e1e9a517dc39b29
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-02-13 15:00:54 +0800

    Add `Triangle` abstraction.

---
+++ ARIA/Core/Geometry/include/ARIA/Triangle.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ARIA/Vec.h"
+
+namespace ARIA {
+
+template <typename T, uint d>
+class Triangle final {
+public:
+private:
+};
+
+} // namespace ARIA
---
+++ ARIA/Core/Geometry/tests/Triangle.cpp
@@ -0,0 +1,9 @@
+#include "ARIA/Triangle.h"
+
+#include <gtest/gtest.h>
+
+namespace ARIA {
+
+TEST(Triangle, Base) {}
+
+} // namespace ARIA
