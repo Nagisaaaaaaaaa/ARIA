dcee3992e55d38f2c20c505347761f5b08ea8ddb
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-19 18:16:12 +0800

    .

--- ARIA/Core/Geometry/include/ARIA/CollisionDetection.h
+++ ARIA/Core/Geometry/include/ARIA/CollisionDetection.h
@@ -6,7 +6,17 @@
 namespace ARIA {
 
 template <typename T, uint d>
-[[nodiscard]] ARIA_HOST_DEVICE static bool SATImpl(const AABB<T, d> &aabb, const Triangle<T, d> &tri) {
+[[nodiscard]] ARIA_HOST_DEVICE bool SATImpl(const AABB<T, d> &aabb, const Triangle<T, d> &tri) {
+  using VecDT = Vec<T, d>;
+
+  VecDT c = aabb.center();
+  VecDT e = aabb.diagonal() / 2_R;
+
+  Triangle v = tri;
+  ForEach<3>([&]<auto i>() { v[i] -= c; });
+
+  std::array<VecDT, 3> f;
+  ForEach<3>([&]<auto i>() { f[i] = v[(i + 1) % 3] - v[i]; });
 }
 
 } // namespace ARIA
