2baf7c88ddfdfcd6076e7498998a4aeaeb7816d6
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-21 19:42:45 +0800

    .

--- ARIA/Core/Geometry/tests/CollisionDetection.cpp
+++ ARIA/Core/Geometry/tests/CollisionDetection.cpp
@@ -13,6 +13,28 @@ ARIA_HOST_DEVICE bool IsIn(const Vec<T, d> &p, const AABB<T, d> &aabb) {
   return res;
 }
 
+template <uint division, typename T, typename F>
+ARIA_HOST_DEVICE void ForEachDivision(const Triangle3<T> &tri, const F &f) {
+  using Vec3T = Vec3<T>;
+
+  if constexpr (division == 0) {
+    f(tri);
+  } else {
+    Vec3T p0 = tri[0];
+    Vec3T p1 = tri[1];
+    Vec3T p2 = tri[2];
+
+    Vec3T p01 = (p0 + p1) / 2;
+    Vec3T p12 = (p1 + p2) / 2;
+    Vec3T p20 = (p2 + p0) / 2;
+
+    ForEachDivision<division - 1>({p0, p01, p20}, f);
+    ForEachDivision<division - 1>({p01, p1, p12}, f);
+    ForEachDivision<division - 1>({p01, p12, p20}, f);
+    ForEachDivision<division - 1>({p20, p12, p2}, f);
+  }
+}
+
 } // namespace
 
 TEST(CollisionDetection, Base) {}
