770f58c24c6556b10f194c597c62ac995a46e4c8
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-21 19:56:12 +0800

    .

--- ARIA/Core/Geometry/tests/CollisionDetection.cpp
+++ ARIA/Core/Geometry/tests/CollisionDetection.cpp
@@ -35,6 +35,15 @@ void ForEachDivision(uint division, const Triangle3<T> &tri, const F &f) {
   }
 }
 
+template <typename T>
+bool Collide(const AABB3<T> &aabb, const Triangle3<T> &tri) {
+  bool collide = false;
+  ForEachDivision(16, tri, [&collide](const Triangle3<T> &t) {
+    collide = collide || IsIn(t[0], aabb) || IsIn(t[1], aabb) || IsIn(t[2], aabb);
+  });
+  return collide;
+}
+
 } // namespace
 
 TEST(CollisionDetection, Base) {}
