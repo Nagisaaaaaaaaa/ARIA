5e5bcb1d69e8e2e1e2dc9a5228ddf3eb5d02540c
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-24 15:55:28 +0800

    Add commonly used utils to SAT.

--- ARIA/Core/Geometry/include/ARIA/detail/CollisionDetectionImpl.h
+++ ARIA/Core/Geometry/include/ARIA/detail/CollisionDetectionImpl.h
@@ -8,11 +8,35 @@ namespace ARIA {
 
 namespace collision_detection::detail {
 
+//! `Tec` will be intensively used to ensure that
+//! expressions will be truly simplified at compile-time.
+
+namespace D2 {
+
+static ARIA_CONST constexpr Tec u0{1_I, 0_I};
+static ARIA_CONST constexpr Tec u1{0_I, 1_I};
+static ARIA_CONST constexpr Tup u{u0, u1};
+
+} // namespace D2
+
+namespace D3 {
+
+static ARIA_CONST constexpr Tec u0{1_I, 0_I, 0_I};
+static ARIA_CONST constexpr Tec u1{0_I, 1_I, 0_I};
+static ARIA_CONST constexpr Tec u2{0_I, 0_I, 1_I};
+static ARIA_CONST constexpr Tup u{u0, u1, u2};
+
+} // namespace D3
+
+//
+//
+//
 //! The following SAT algorithms are implemented based on
 //! 2005, Christer Ericson, Real-Time Collision Detection, Chapter 5.2.9 Testing AABB Against Triangle.
 
 template <typename T>
 [[nodiscard]] ARIA_HOST_DEVICE bool SAT(const AABB2<T> &aabb, const LineSegment2<T> &seg) {
+  using namespace D2;
   using Vec2T = Vec2<T>;
 
   Vec2T c = aabb.center();
@@ -22,15 +46,6 @@ template <typename T>
   ForEach<2>([&]<auto i>() { v[i] -= c; });
 
   Vec2T f = v[1] - v[0];
-
-  //! `Tec` is intensively used in the following codes to ensure that
-  //! expressions will be simplified at compile-time.
-  constexpr C<T(0)> _0;
-  constexpr C<T(1)> _1;
-
-  constexpr Tec u0{_1, _0};
-  constexpr Tec u1{_0, _1};
-  constexpr Tup u{u0, u1};
 
   // Test whether the given `axis` is a separating axis (SA).
   auto isSA = [&](const auto &axis) {
@@ -56,6 +71,7 @@ template <typename T>
 
 template <typename T>
 [[nodiscard]] ARIA_HOST_DEVICE bool SAT(const AABB3<T> &aabb, const Triangle3<T> &tri) {
+  using namespace D3;
   using Vec3T = Vec3<T>;
 
   Vec3T c = aabb.center();
@@ -66,16 +82,6 @@ template <typename T>
 
   std::array<Vec3T, 3> f;
   ForEach<3>([&]<auto i>() { f[i] = v[(i + 1) % 3] - v[i]; });
-
-  //! `Tec` is intensively used in the following codes to ensure that
-  //! expressions will be simplified at compile-time.
-  constexpr C<T(0)> _0;
-  constexpr C<T(1)> _1;
-
-  constexpr Tec u0{_1, _0, _0};
-  constexpr Tec u1{_0, _1, _0};
-  constexpr Tec u2{_0, _0, _1};
-  constexpr Tup u{u0, u1, u2};
 
   // Test whether the given `axis` is a separating axis (SA).
   auto isSA = [&](const auto &axis) {
