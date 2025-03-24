45c47a534c36b55ae814e9d97d0fb2ee38427188
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-03-24 14:50:32 +0800

    .

--- ARIA/Core/Geometry/include/ARIA/CollisionDetection.h
+++ ARIA/Core/Geometry/include/ARIA/CollisionDetection.h
@@ -12,11 +12,25 @@
 
 namespace ARIA {
 
+/// \brief Test whether the given `aabb` and `seg` are intersecting.
+///
+/// \example ```cpp
+/// AABB2r aabb{...};
+/// LineSegment2r seg{...};
+/// bool collides = DetectCollision(aabb, seg);
+/// ```
 template <typename T>
 [[nodiscard]] ARIA_HOST_DEVICE bool DetectCollision(const AABB2<T> &aabb, const LineSegment2<T> &seg) {
   return collision_detection::detail::SAT(aabb, seg);
 }
 
+/// \brief Test whether the given `seg` and `aabb` are intersecting.
+///
+/// \example ```cpp
+/// LineSegment2r seg{...};
+/// AABB2r aabb{...};
+/// bool collides = DetectCollision(seg, aabb);
+/// ```
 template <typename T>
 [[nodiscard]] ARIA_HOST_DEVICE bool DetectCollision(const LineSegment2<T> &seg, const AABB2<T> &aabb) {
   return DetectCollision(aabb, seg);
