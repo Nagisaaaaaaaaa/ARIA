da08d8397829431f4df88368ad4e27c10cc87ddc
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-05-18 13:47:18 +0800

    .

--- ARIA/Core/Math/include/ARIA/detail/VecImpl.h
+++ ARIA/Core/Math/include/ARIA/detail/VecImpl.h
@@ -44,7 +44,28 @@ static constexpr bool is_vec_s_v = is_vec_s<T, s>::value;
 //
 //! `Eigen::Vector` is just a special case of `Eigen::Matrix`, so
 //! they share the same member functions.
+#define __ARIA_PROP_AND_SUB_PROP_INCOMPLETE_PREFAB_MEMBERS_VEC __ARIA_PROP_AND_SUB_PROP_INCOMPLETE_PREFAB_MEMBERS_MAT
+
 #define __ARIA_PROP_AND_SUB_PROP_PREFAB_MEMBERS_VEC __ARIA_PROP_AND_SUB_PROP_PREFAB_MEMBERS_MAT
+
+//
+//
+//
+#define __ARIA_PROP_INCOMPLETE_PREFAB_VEC(accessGet, accessSet, specifiers, type, /*propName,*/...)                    \
+  static_assert(ARIA::vec::detail::is_vec_v<std::decay_t<type>>,                                                       \
+                "Type of the property should be `class Vec` in order to use this prefab");                             \
+                                                                                                                       \
+  ARIA_PROP_BEGIN(accessGet, accessSet, specifiers, type, /*propName,*/ __VA_ARGS__);                                  \
+  __ARIA_PROP_AND_SUB_PROP_INCOMPLETE_PREFAB_MEMBERS_VEC(specifiers, type);                                            \
+  ARIA_PROP_END
+
+#define __ARIA_SUB_PROP_INCOMPLETE_PREFAB_VEC(specifiers, type, /*propName,*/...)                                      \
+  static_assert(ARIA::vec::detail::is_vec_v<std::decay_t<type>>,                                                       \
+                "Type of the property should be `class Vec` in order to use this prefab");                             \
+                                                                                                                       \
+  ARIA_SUB_PROP_BEGIN(specifiers, type, /*propName,*/ __VA_ARGS__);                                                    \
+  __ARIA_PROP_AND_SUB_PROP_INCOMPLETE_PREFAB_MEMBERS_VEC(specifiers, type);                                            \
+  ARIA_PROP_END
 
 #define __ARIA_PROP_PREFAB_VEC(accessGet, accessSet, specifiers, type, /*propName,*/...)                               \
   static_assert(ARIA::vec::detail::is_vec_v<std::decay_t<type>>,                                                       \
